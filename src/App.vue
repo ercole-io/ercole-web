057456c02c18664438ef3400fdaad666004dd486
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2020-05-04 16:26:43 -0300

    Basic component structure and basic header layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,30 @@
 <template>
   <div id="app">
-    <router-view />
+    <appHeader />
+    <div class="columns">
+      <div class="column is-one-fifth">
+        <appMenu />
+      </div>
+      <div class="column">
+        <router-view />
+      </div>
+    </div>
+    <appFooter />
   </div>
 </template>
+
+<script>
+import Header from '@/components/Header.vue'
+import Menu from '@/components/Menu.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  components: {
+    appHeader: Header,
+    appMenu: Menu,
+    appFooter: Footer
+  }
+}
+</script>
 
 <style lang="scss"></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,31 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Dashboard from '../views/Dashboard.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Dashboard',
+    component: Dashboard,
+    meta: {
+      title: 'Ercole - Dashboard'
+    }
   }
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
+  linkActiveClass: 'active',
+  linkExactActiveClass: 'exact-active'
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title
+  next()
 })
 
 export default router
