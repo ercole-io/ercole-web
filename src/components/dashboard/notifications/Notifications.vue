fbd96c4db2ddf29c5eaf84743ff2ee049c3cc15d
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2020-05-08 15:48:43 -0300

    Reorganizing notifications components

---
+++ src/components/dashboard/notifications/Notifications.vue
@@ -0,0 +1,35 @@
+<template>
+  <BoxContent :title="$t('dashboard.pageTitle.notificatons')">
+    <Notification>
+      <Agents slot="notify" title="Agents" />
+    </Notification>
+
+    <Notification>
+      <Licenses slot="notify" title="Licenses" />
+    </Notification>
+
+    <Notification>
+      <System slot="notify" title="System" />
+    </Notification>
+  </BoxContent>
+</template>
+
+<script>
+import BoxContent from '@/components/common/BoxContent.vue'
+import Notification from '@/components/dashboard/notifications/Notification.vue'
+import Agents from '@/components/dashboard/notifications/Agents.vue'
+import Licenses from '@/components/dashboard/notifications/Licenses.vue'
+import System from '@/components/dashboard/notifications/System.vue'
+
+export default {
+  components: {
+    BoxContent,
+    Notification,
+    Agents,
+    Licenses,
+    System
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
