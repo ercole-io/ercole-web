9abbc1b20ef96d544fa582a128cae3990316165b
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2020-05-11 14:23:49 -0300

    Defining Vue Chartkick

--- src/components/common/charts/PieChart.vue
+++ src/components/common/charts/PieChart.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>Pie Chart</div>
+  <pie-chart
+    width="250px"
+    height="250px"
+    legend="bottom"
+    :data="[
+      ['Item 1', 50],
+      ['Item 2', 30],
+      ['Item 3', 10],
+      ['Item 4', 20]
+    ]"
+  ></pie-chart>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ import store from './store'
 
 import Buefy from 'buefy'
 import i18n from './i18n'
+import Chartkick from 'vue-chartkick'
+import Chart from 'chart.js'
 
 Vue.use(Buefy)
+Vue.use(Chartkick.use(Chart))
 
 Vue.config.productionTip = false
 
