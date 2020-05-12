ef947a2c472e8c72b55853937b92f5f2e17b39ed
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2020-05-12 17:33:42 -0300

    Total targets component layout

--- src/components/dashboard/TotalTargets.vue
+++ src/components/dashboard/TotalTargets.vue
@@ -1,6 +1,29 @@
 <template>
   <BoxContent :title="$t('dashboard.pageTitle.totalTargets')">
-    total targets
+    <div class="total-targets">
+      <div class="total-targets-col">
+        <div>
+          Agents discovered
+        </div>
+        <div>
+          Percentage of compliance
+        </div>
+        <div>
+          Money missing
+        </div>
+      </div>
+      <div class="total-targets-col">
+        <div>
+          <span class="has-text-weight-semibold is-size-5">100</span>/150
+        </div>
+        <div
+          class="has-text-weight-semibold is-size-5 has-background-grey-lighter"
+        >
+          80%
+        </div>
+        <div class="has-text-weight-semibold is-size-5">-87.000 €</div>
+      </div>
+    </div>
   </BoxContent>
 </template>
 
@@ -25,10 +48,32 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.header {
-  display: none;
-}
-.row {
-  border: none;
+.total-targets {
+  display: flex;
+  font-size: 13px;
+
+  .total-targets-col {
+    width: 50%;
+
+    div {
+      height: 25px;
+      display: flex;
+      align-items: center;
+      margin: 12px 0;
+    }
+  }
+
+  .total-targets-col:first-child {
+    div {
+      justify-content: left;
+      line-height: 14px;
+    }
+  }
+
+  .total-targets-col:last-child {
+    div {
+      justify-content: center;
+    }
+  }
 }
 </style>
