ff8b02f8734ac3c40d86663befb563fac7742721
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2020-05-13 16:37:20 -0300

    Hosts page basic layout with mocked data

---
+++ src/components/Hosts/HostsTable.vue
@@ -0,0 +1,217 @@
+<template>
+  <BoxContent>
+    <b-table
+      :data="isEmpty ? [] : data"
+      :columns="columns"
+      :paginated="isPaginated"
+      :per-page="perPage"
+      :current-page.sync="currentPage"
+      :pagination-size="paginationSize"
+      :pagination-simple="isPaginationSimple"
+      :pagination-position="paginationPosition"
+      :default-sort-direction="defaultSortDirection"
+      :sort-icon="sortIcon"
+      :sort-icon-size="sortIconSize"
+      :striped="isStriped"
+      :narrowed="isNarrowed"
+      default-sort="hostname"
+      aria-next-label="Next page"
+      aria-previous-label="Previous page"
+      aria-page-label="Page"
+      aria-current-label="Current page"
+      :mobile-cards="true"
+      :loading="isLoading"
+      :scrollable="true"
+    >
+      <b-input
+        slot="searchable"
+        slot-scope="props"
+        v-model="props.filters[props.column.field]"
+        placeholder="Search..."
+        icon="magnify"
+        size="is-small"
+      />
+
+      <template slot="empty">
+        <section class="section">
+          <div class="content has-text-grey has-text-centered">
+            <p>
+              <b-icon icon="emoticon-sad" size="is-large"> </b-icon>
+            </p>
+            <p>Nothing here.</p>
+          </div>
+        </section>
+      </template>
+
+      <template slot="bottom-left">
+        <b-field grouped group-multiline>
+          <b-select v-model="perPage" :disabled="!isPaginated" size="is-small">
+            <option value="5">5 per page</option>
+            <option value="10">10 per page</option>
+            <option value="15">15 per page</option>
+            <option value="20">20 per page</option>
+            <option value="50">50 per page</option>
+          </b-select>
+        </b-field>
+      </template>
+    </b-table>
+  </BoxContent>
+</template>
+
+<script>
+const data = require('@/components/Hosts/hosts.json')
+import BoxContent from '@/components/common/BoxContent.vue'
+
+export default {
+  components: {
+    BoxContent
+  },
+  data() {
+    return {
+      data: data,
+      columns: [
+        {
+          field: 'hostname',
+          label: 'Host',
+          searchable: true,
+          sortable: true
+        },
+        {
+          field: 'env',
+          label: 'Env',
+          centered: true,
+          searchable: true,
+          sortable: true
+        },
+        {
+          field: 'dbs',
+          label: 'DBs',
+          centered: true,
+          searchable: true,
+          sortable: true
+        },
+        {
+          field: 'technologie',
+          label: 'Tech',
+          centered: true,
+          searchable: true,
+          sortable: true
+        },
+        {
+          field: 'platform',
+          label: 'Platform',
+          centered: true,
+          searchable: true,
+          sortable: true
+        },
+        {
+          field: 'cluster',
+          label: 'Cluster',
+          centered: true,
+          searchable: true,
+          sortable: true
+        },
+        {
+          field: 'physical',
+          label: 'Physical',
+          centered: true,
+          searchable: true,
+          sortable: true
+        },
+        {
+          field: 'os',
+          label: 'OS',
+          centered: true,
+          searchable: true,
+          sortable: true
+        },
+        {
+          field: 'kernel',
+          label: 'kernel',
+          centered: true,
+          searchable: true,
+          sortable: true
+        },
+        {
+          field: 'memory',
+          label: 'Mem.',
+          centered: true,
+          searchable: true,
+          sortable: true
+        },
+        {
+          field: 'swap',
+          label: 'Swap',
+          centered: true,
+          searchable: true,
+          sortable: true
+        },
+        {
+          field: 'clust',
+          label: 'Clust',
+          centered: true,
+          searchable: true,
+          sortable: true
+        },
+        {
+          field: 'model',
+          label: 'Model',
+          centered: true,
+          searchable: true,
+          sortable: true
+        },
+        {
+          field: 'threa',
+          label: 'Threa',
+          centered: true,
+          searchable: true,
+          sortable: true
+        },
+        {
+          field: 'cores',
+          label: 'Cores',
+          centered: true,
+          searchable: true,
+          sortable: true
+        },
+        {
+          field: 'socket',
+          label: 'Socket',
+          centered: true,
+          searchable: true,
+          sortable: true
+        },
+        {
+          field: 'version',
+          label: 'Version',
+          centered: true,
+          searchable: true,
+          sortable: true
+        },
+        {
+          field: 'last',
+          label: 'Last',
+          centered: true,
+          searchable: true,
+          sortable: true
+        }
+      ],
+      isLoading: false,
+      isEmpty: false,
+      isStriped: true,
+      isNarrowed: true,
+      isPaginated: true,
+      isPaginationSimple: false,
+      paginationPosition: 'bottom',
+      paginationSize: 'is-small',
+      defaultSortDirection: 'asc',
+      sortIcon: 'chevron-up',
+      sortIconSize: 'is-small',
+      currentPage: 1,
+      perPage: 10
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Hosts.vue
+++ src/views/Hosts.vue
@@ -1,115 +1,18 @@
 <template>
   <section>
-    <b-field grouped group-multiline>
-      <b-select v-model="defaultSortDirection">
-        <option value="asc">Default sort direction: ASC</option>
-        <option value="desc">Default sort direction: DESC</option>
-      </b-select>
-      <b-select v-model="perPage" :disabled="!isPaginated">
-        <option value="5">5 per page</option>
-        <option value="10">10 per page</option>
-        <option value="15">15 per page</option>
-        <option value="20">20 per page</option>
-      </b-select>
-      <div class="control">
-        <button
-          class="button"
-          @click="currentPage = 2"
-          :disabled="!isPaginated"
-        >
-          Set page to 2
-        </button>
-      </div>
-      <div class="control is-flex">
-        <b-switch v-model="isPaginated">Paginated</b-switch>
-      </div>
-      <div class="control is-flex">
-        <b-switch v-model="isPaginationSimple" :disabled="!isPaginated"
-          >Simple pagination</b-switch
-        >
-      </div>
-      <b-select v-model="paginationPosition" :disabled="!isPaginated">
-        <option value="bottom">bottom pagination</option>
-        <option value="top">top pagination</option>
-        <option value="both">both</option>
-      </b-select>
-      <b-select v-model="sortIcon">
-        <option value="arrow-up">Arrow sort icon</option>
-        <option value="menu-up">Caret sort icon</option>
-        <option value="chevron-up">Chevron sort icon </option>
-      </b-select>
-      <b-select v-model="sortIconSize">
-        <option value="is-small">Small sort icon</option>
-        <option value="">Regular sort icon</option>
-        <option value="is-medium">Medium sort icon</option>
-        <option value="is-large">Large sort icon</option>
-      </b-select>
-    </b-field>
-
-    <b-table
-      :data="data"
-      :paginated="isPaginated"
-      :per-page="perPage"
-      :current-page.sync="currentPage"
-      :pagination-simple="isPaginationSimple"
-      :pagination-position="paginationPosition"
-      :default-sort-direction="defaultSortDirection"
-      :sort-icon="sortIcon"
-      :sort-icon-size="sortIconSize"
-      default-sort="user.first_name"
-      aria-next-label="Next page"
-      aria-previous-label="Previous page"
-      aria-page-label="Page"
-      aria-current-label="Current page"
-    >
-      <template slot-scope="props">
-        <b-table-column field="id" label="ID" width="40" sortable numeric>
-          {{ props.row.id }}
-        </b-table-column>
-
-        <b-table-column field="user.first_name" label="First Name" sortable>
-          {{ props.row.user.first_name }}
-        </b-table-column>
-
-        <b-table-column field="user.last_name" label="Last Name" sortable>
-          {{ props.row.user.last_name }}
-        </b-table-column>
-
-        <b-table-column field="date" label="Date" sortable centered>
-          <span class="tag is-success">
-            {{ new Date(props.row.date).toLocaleDateString() }}
-          </span>
-        </b-table-column>
-
-        <b-table-column label="Gender">
-          <span>
-            <b-icon
-              pack="fas"
-              :icon="props.row.gender === 'Male' ? 'mars' : 'venus'"
-            >
-            </b-icon>
-            {{ props.row.gender }}
-          </span>
-        </b-table-column>
-      </template>
-    </b-table>
+    <Filters />
+    <HostsTable />
   </section>
 </template>
 
 <script>
+import Filters from '@/components/common/Filters.vue'
+import HostsTable from '@/components/Hosts/HostsTable.vue'
+
 export default {
-  data() {
-    return {
-      // data,
-      isPaginated: true,
-      isPaginationSimple: false,
-      paginationPosition: 'bottom',
-      defaultSortDirection: 'asc',
-      sortIcon: 'arrow-up',
-      sortIconSize: 'is-small',
-      currentPage: 1,
-      perPage: 5
-    }
+  components: {
+    Filters,
+    HostsTable
   }
 }
 </script>
