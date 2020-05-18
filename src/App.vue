19e316a062e0fa003d59ee9f65bee066bfcd573d
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2020-05-18 15:35:54 -0300

    Creating a 2 different layout (default and simple)

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,23 @@
 <template>
   <div id="app">
-    <appHeader />
-    <main class="main">
-      <div class="columns">
-        <div class="column is-2">
-          <appMenu />
-        </div>
-        <div class="column is-10">
-          <router-view :key="$route.fullPath" class="content" />
-        </div>
-      </div>
-    </main>
-    <appFooter />
+    <component :is="layout">
+      <router-view :key="$route.fullPath" />
+    </component>
   </div>
 </template>
 
 <script>
-import Header from '@/components/header/Header.vue'
-import Menu from '@/components/Menu.vue'
-import Footer from '@/components/Footer.vue'
+const default_layout = 'default'
 
 export default {
-  components: {
-    appHeader: Header,
-    appMenu: Menu,
-    appFooter: Footer
+  computed: {
+    layout() {
+      return (this.$route.meta.layout || default_layout) + '-layout'
+    }
   }
 }
 </script>
 
 <style lang="scss">
 @import '@/styles/styles.scss';
-
-#app {
-  display: flex;
-  min-height: 100vh;
-  flex-direction: column;
-}
-
-.main {
-  flex: 1;
-  padding: 0.75rem;
-}
-
-.content {
-  padding: 0.75rem;
-}
 </style>
---
+++ src/layouts/Default.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="app">
+    <appHeader />
+    <main class="main">
+      <div class="columns">
+        <div class="column is-2">
+          <appMenu />
+        </div>
+        <div class="column is-10">
+          <div class="content">
+            <slot />
+          </div>
+        </div>
+      </div>
+    </main>
+    <appFooter />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/header/Header.vue'
+import Menu from '@/components/Menu.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  components: {
+    appHeader: Header,
+    appMenu: Menu,
+    appFooter: Footer
+  }
+}
+</script>
+
+<style lang="scss">
+.app {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+
+.main {
+  flex: 1;
+  padding: 0.75rem;
+}
+
+.content {
+  padding: 0.75rem;
+}
+</style>
