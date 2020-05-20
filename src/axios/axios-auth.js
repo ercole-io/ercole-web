061cc23f9cd508b5cd4c3ec04241f66ed6d25c4c
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2020-05-20 17:08:07 -0300

    Setting up dev and prod env

--- src/axios/axios-auth.js
+++ src/axios/axios-auth.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 import store from '../store/index.js'
 
+const api = process.env.VUE_APP_API
+
 const instance = axios.create({
-  baseURL: ' http://dev.ercole.io/api/user'
+  baseURL: `${api}/user`
 })
 
 instance.interceptors.request.use(config => {
