035db3124fdb7a16b3860e6f00aea359ff00a299
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2020-05-28 11:03:07 -0300

    Removing err from dashboard

--- src/store/modules/hosts.js
+++ src/store/modules/hosts.js
@@ -2,18 +2,25 @@ import axiosDefault from '../../axios/axios-default'
 import router from '../../router/index.js'
 
 export const state = () => ({
-  hosts: null
+  hosts: null,
+  hostByName: null
 })
 
 export const getters = {
   getAllHosts: state => {
     return state.hosts
+  },
+  getHostName: state => {
+    return state.hostByName
   }
 }
 
 export const mutations = {
   SET_HOSTS: (state, payload) => {
     state.hosts = payload
+  },
+  SET_HOST_ID: (state, payload) => {
+    state.hostByName = payload
   }
 }
 
@@ -31,6 +38,28 @@ export const actions = {
         })
         .catch(err => {
           reject(err)
+          dispatch('offLoading')
+          if (err.response.status === 401) {
+            router.replace('/login')
+          }
+        })
+    })
+  },
+  getHostByName({ commit, dispatch }, hostname) {
+    return new Promise((resolve, reject) => {
+      axiosDefault
+        .get(`/hosts/${hostname}`)
+        .then(res => {
+          console.log(res)
+          commit('SET_HOST_ID', res.data)
+          resolve(res)
+        })
+        .then(() => {
+          dispatch('offLoading')
+        })
+        .catch(err => {
+          reject(err)
+          dispatch('offLoading')
           if (err.response.status === 401) {
             router.replace('/login')
           }
