e9a22c12983c2f92f5b4cda81e661d34e19e7dd5
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2020-06-03 16:45:57 -0300

    Looping database tabs on hosts details

--- src/components/hosts/HostDetailsData.vue
+++ src/components/hosts/HostDetailsData.vue
@@ -1,9 +1,33 @@
 <template>
-  <div></div>
+  <b-tabs
+    v-model="activeTab"
+    :multiline="multiline"
+    size="is-small"
+    type="is-primary"
+  >
+    <template v-for="tab in hostData">
+      <b-tab-item :key="tab.UniqueName" :label="tab.Name">
+        {{ tab.Name }}
+      </b-tab-item>
+    </template>
+  </b-tabs>
 </template>
 
 <script>
-export default {}
+export default {
+  props: {
+    hostData: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      activeTab: 0,
+      multiline: true
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
