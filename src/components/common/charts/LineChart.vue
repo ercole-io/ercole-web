fd8907104704ca64b583913df2e301760f82503a
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2020-06-03 12:12:54 -0300

    Creating a line chart

---
+++ src/components/common/charts/LineChart.vue
@@ -0,0 +1,16 @@
+<template>
+  <line-chart :data="lineChartData" />
+</template>
+
+<script>
+export default {
+  props: {
+    lineChartData: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
