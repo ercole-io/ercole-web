418657517420ea20dfcb58a37d8055dc998a736d
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2020-06-08 16:43:36 -0300

    Host details patches tab

---
+++ src/components/hosts/hostDetails/databases/DbPatches.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-table :data="patches" :hideSortIcons="true" class="vTable-custom">
+    <thead slot="head">
+      <tr class="has-background-grey-lighter">
+        <v-th sortKey="Action">Action</v-th>
+        <v-th sortKey="Version">Version</v-th>
+        <v-th sortKey="Description">Description</v-th>
+        <v-th sortKey="PatchID">Patch</v-th>
+        <v-th sortKey="Date">Date</v-th>
+      </tr>
+    </thead>
+    <tbody slot="body" slot-scope="{ displayData }">
+      <v-tr v-for="(row, index) in displayData" :key="index" :row="row">
+        <td>{{ row.Action }}</td>
+        <td>{{ row.Version }}</td>
+        <td>{{ row.Description }}</td>
+        <td>{{ row.PatchID }}</td>
+        <td>{{ row.Date | formatDate }}</td>
+      </v-tr>
+    </tbody>
+  </v-table>
+</template>
+
+<script>
+export default {
+  props: {
+    patches: {
+      type: Array,
+      default: null
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
