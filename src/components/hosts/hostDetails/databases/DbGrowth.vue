25daedadf49762714c1c416ae38e36193d5c8e55
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2020-06-09 09:29:25 -0300

    Fixing some problems in host details dbgrowth tab

--- src/components/hosts/hostDetails/databases/DbGrowth.vue
+++ src/components/hosts/hostDetails/databases/DbGrowth.vue
@@ -1,5 +1,5 @@
 <template>
-  <LineChart chartId="dbGrowth" :lineChartData="lineData" />
+  <LineChart :chartId="growthId" :lineChartData="growth" />
 </template>
 
 <script>
@@ -7,6 +7,10 @@ import LineChart from '@/components/common/charts/LineChart.vue'
 
 export default {
   props: {
+    growthId: {
+      type: String,
+      required: true
+    },
     growth: {
       type: Array,
       default: null
@@ -14,38 +18,6 @@ export default {
   },
   components: {
     LineChart
-  },
-  data() {
-    return {
-      lineData: [
-        {
-          name: 'Value 1',
-          data: {
-            '2013-02-10': 30,
-            '2013-02-17': 30,
-            '2013-02-24': 30,
-            '2013-03-03': 10,
-            '2013-03-10': 40,
-            '2013-03-17': 30,
-            '2013-03-24': 20,
-            '2013-03-31': 30
-          }
-        },
-        {
-          name: 'Value 2',
-          data: {
-            '2013-02-10': 0,
-            '2013-02-17': 10,
-            '2013-02-24': 0,
-            '2013-03-03': 50,
-            '2013-03-10': 20,
-            '2013-03-17': 10,
-            '2013-03-24': 0,
-            '2013-03-31': 70
-          }
-        }
-      ]
-    }
   }
 }
 </script>
