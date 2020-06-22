aaed82cd165eead5f4ff8e7fcbbfc57bcc5452c5
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2020-06-22 16:00:33 -0300

    Creating a noConte component

---
+++ src/components/common/NoContent.vue
@@ -0,0 +1,25 @@
+<template>
+  <p class="no-content is-size-7 has-text-weight-medium">
+    {{ noContentText }}
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    noContentText: {
+      type: String,
+      default: 'There are no content to show'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.no-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 90%;
+}
+</style>
