d96a7c1a0d7b4b32abde98e6bb9770f8edc4fe29
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2020-06-23 13:44:19 -0300

    Creating a card and card list component

---
+++ src/components/common/Card.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="card">
+    <header class="card-header">
+      <p class="card-header-title card-title">
+        {{ cardTitle }}
+      </p>
+    </header>
+    <div class="card-content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['cardTitle']
+}
+</script>
+
+<style lang="scss" scoped>
+.card-title {
+  font-weight: 500;
+  padding: 0.5rem 1rem;
+  font-size: 0.9em;
+}
+
+.card-content {
+  padding: 1rem;
+  font-size: 0.8em;
+}
+</style>
---
+++ src/components/common/CardList.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul class="card-list">
+    <li v-for="item in list" :key="item.name">
+      <span>{{ item.name }}</span>
+      <span>{{ item.value }}</span>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    list: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.card-list {
+  li {
+    display: flex;
+    justify-content: space-between;
+    font-size: 0.9em;
+    padding: 0.2em 1em;
+
+    &:nth-child(odd) {
+      background: #f1f1f1;
+    }
+
+    span:first-child {
+      font-weight: 500;
+    }
+  }
+}
+</style>
