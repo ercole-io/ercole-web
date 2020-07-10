ab1bfcc25026145d66417ae4cfe60f86b1590bd6
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2020-07-10 15:57:25 -0300

    refactoring and crating some reusable components

---
+++ src/components/common/BottomTable.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="is-size-7 has-text-weight-medium has-text-centered">
+      <slot name="info" />
+    </div>
+
+    <div
+      class="is-flex"
+      style="justify-content: space-between; margin-bottom: 10px"
+    >
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
