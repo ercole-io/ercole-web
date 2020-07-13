d4f7195dff522b16e8887a00360c1297b108d942
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2020-07-13 11:11:58 -0300

    Fixing wrong text on select per page component

--- src/components/common/SelectPerPage.vue
+++ src/components/common/SelectPerPage.vue
@@ -7,7 +7,7 @@
       <option value="20" v-if="totalItem > 20">20 per page</option>
       <option value="25" v-if="totalItem > 25">25 per page</option>
       <option value="50" v-if="totalItem > 50">50 per page</option>
-      <option :value="totalItem">All Hosts</option>
+      <option :value="totalItem">All Items</option>
     </b-select>
   </b-field>
 </template>
