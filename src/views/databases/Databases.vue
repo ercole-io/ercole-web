6dc6e504610fa2d7bd5ca66c345102cc13ac4238
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2020-08-17 20:13:55 -0300

    Fixing issue #189

--- src/views/databases/Databases.vue
+++ src/views/databases/Databases.vue
@@ -164,7 +164,7 @@ export default {
     ...mapActions(['getDatabases']),
     mapBooleanIcon(value) {
       return value
-        ? ['check-circle', 'fas', 'is-success', 'yes']
+        ? ['circle', 'fas', 'is-success', 'yes']
         : ['circle', 'fas', 'is-danger', 'no']
     }
   },
