2fa7fafdfcd37c8a07b993b1419e65aa8f455dfe
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2020-08-18 19:57:51 -0300

    Fixing issue #182

---
+++ src/components/common/Table/FilteredResults.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Showing {{ totalItems }} filtered results on this page</div>
+</template>
+
+<script>
+export default {
+  props: {
+    totalItems: {
+      type: Number
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
