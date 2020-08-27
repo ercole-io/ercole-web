21e1c099b05b38336fa887cce3a5239b5fe4b0a0
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2020-08-27 18:42:36 -0300

    Showing the right notification alerts on host details and filtering on click when redirect to the alerts page

--- src/components/hosts/hostDetails/NotificationsInfo.vue
+++ src/components/hosts/hostDetails/NotificationsInfo.vue
@@ -1,9 +1,42 @@
 <template>
   <span class="has-text-weight-medium	is-size-7">
     {{ info.total }} notifications from the last week:
-    <span class="has-text-weight-light">Agents:</span> {{ info.agents }}
-    <span class="has-text-weight-light"> | Licenses:</span> {{ info.licenses }}
-    <span class="has-text-weight-light"> | Systems:</span> {{ info.systems }}
+    <router-link
+      v-if="info.agents > 0"
+      :to="{ name: 'alerts', params: { type: info.hostname, flag: 'AGENT' } }"
+      tag="a"
+    >
+      <span class="has-text-weight-light">Agents:</span> {{ info.agents }}
+    </router-link>
+    <span v-if="info.agents === 0">
+      <span class="has-text-weight-light">Agents:</span>
+      {{ info.agents }}
+    </span>
+    |
+    <router-link
+      v-if="info.licenses > 0"
+      :to="{ name: 'alerts', params: { type: info.hostname, flag: 'LICENSE' } }"
+      tag="a"
+    >
+      <span class="has-text-weight-light">Licenses:</span>
+      {{ info.licenses }}
+    </router-link>
+    <span v-if="info.licenses === 0">
+      <span class="has-text-weight-light">Licenses:</span>
+      {{ info.licenses }}
+    </span>
+    |
+    <router-link
+      v-if="info.engine > 0"
+      :to="{ name: 'alerts', params: { type: info.hostname, flag: 'ENGINE' } }"
+      tag="a"
+    >
+      <span class="has-text-weight-light">Engine:</span> {{ info.engine }}
+    </router-link>
+    <span v-if="info.engine === 0">
+      <span class="has-text-weight-light">Engine:</span>
+      {{ info.engine }}
+    </span>
   </span>
 </template>
 
