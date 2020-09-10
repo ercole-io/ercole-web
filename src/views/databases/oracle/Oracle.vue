6a8f11ce19500384e65bab0f3fb54268b2257774
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2020-09-10 14:53:03 -0300

    Fixing same things from issue #217 for the oracle page and fixing a problem with the High Availability chart

--- src/views/databases/oracle/Oracle.vue
+++ src/views/databases/oracle/Oracle.vue
@@ -61,7 +61,7 @@
           <div class="column is-12">
             <Collapse title="Type Of Environment" id="TypeOfEnvironment" isOpen>
               <ColumnChart
-                chartId="environmentType"
+                chartId="et"
                 :columnChartData="getEnvironmentTypeChartDataOracle"
                 chartHeight="150px"
                 stacked
@@ -73,7 +73,7 @@
           <div class="column is-12">
             <Collapse title="Archivelog Mode" id="ArchivelogMode">
               <ColumnChart
-                chartId="archivelog"
+                chartId="al"
                 :columnChartData="getArchivelogChartDataOracle"
                 chartHeight="150px"
                 stacked
@@ -85,7 +85,7 @@
           <div class="column is-12">
             <Collapse title="Disaster Recovery" id="DisasterRecovery">
               <ColumnChart
-                chartId="dataguard"
+                chartId="dr"
                 :columnChartData="getDataguardChartDataOracle"
                 chartHeight="150px"
                 stacked
@@ -95,10 +95,10 @@
         </div>
         <div class="columns">
           <div class="column is-12">
-            <Collapse title="High Reliability" id="HighReliability">
+            <Collapse title="High Availability" id="HighAvailability">
               <ColumnChart
-                chartId="highReliability"
-                :columnChartData="getHighReliabilityChartDataOracle"
+                chartId="ha"
+                :columnChartData="getHaChartDataOracle"
                 chartHeight="150px"
                 stacked
               />
@@ -119,11 +119,11 @@
             <v-th sortKey="hostname">Hostname</v-th>
             <v-th sortKey="environment">Env.</v-th>
             <v-th sortKey="charset">Charset</v-th>
-            <v-th sortKey="memory">Mem.</v-th>
-            <v-th sortKey="datafileSize">DataFile</v-th>
-            <v-th sortKey="segmentsSize">Seg.</v-th>
-            <v-th sortKey="archivelog">A</v-th>
-            <v-th sortKey="dataguard">D</v-th>
+            <v-th sortKey="memory">Memory (GB)</v-th>
+            <v-th sortKey="datafileSize">Datafile size (GB)</v-th>
+            <v-th sortKey="segmentsSize">Segment size (GB)</v-th>
+            <v-th sortKey="archivelog">Archivelog Mode</v-th>
+            <v-th sortKey="dataguard">DR</v-th>
             <v-th sortKey="ha">HA</v-th>
           </template>
 
@@ -133,9 +133,7 @@
             <TdContent :value="rowData.scope.hostname" />
             <TdContent :value="rowData.scope.environment" />
             <TdContent :value="rowData.scope.charset" />
-            <TdContent
-              :value="rowData.scope.memory | formatNumber('0.00', 'GB')"
-            />
+            <TdContent :value="rowData.scope.memory | formatNumber('0.00')" />
             <TdContent :value="rowData.scope.datafileSize" />
             <TdContent :value="rowData.scope.segmentsSize" />
             <TdIcon :value="bindIcon(rowData.scope.archivelog)" />
@@ -209,7 +207,7 @@ export default {
       'getEnvironmentTypeChartDataOracle',
       'getArchivelogChartDataOracle',
       'getDataguardChartDataOracle',
-      'getHighReliabilityChartDataOracle'
+      'getHaChartDataOracle'
     ])
   }
 }
