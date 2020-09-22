f2f9def25c30cd75c2368b35d3f1ce0fffb998c2
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2020-09-22 13:35:32 -0300

    Add license agreement mock

--- src/store/modules/licensesAgreement.js
+++ src/store/modules/licensesAgreement.js
@@ -2,7 +2,58 @@ import axiosDefault from '../../axios/axios-default.js'
 import _ from 'lodash'
 
 export const state = () => ({
-  licensesAgreement: {},
+  licensesAgreement: [
+    {
+      id: 0,
+      agreeNumber: 5051863,
+      partNumber: 'L75978',
+      description: 'Oracle GoldenGate',
+      metrics: 'Processor Perpetual',
+      csi: 19765174,
+      referenceNumber: 103246681,
+      ula: 'no',
+      licenseNumber: 72,
+      userNumber: null,
+      availableNumber: 60,
+      hostAssociated: [
+        {
+          id: 0,
+          hostname: 'erclin6db18',
+          usedLicenses: 4
+        },
+        {
+          id: 0,
+          hostname: 'erclin7dbx',
+          usedLicenses: 8
+        }
+      ]
+    },
+    {
+      id: 2,
+      agreeNumber: 5051863,
+      partNumber: 'L75978',
+      description: 'Oracle GoldenGate',
+      metrics: 'Processor Perpetual',
+      csi: 19765174,
+      referenceNumber: 103246681,
+      ula: 'no',
+      licenseNumber: 72,
+      userNumber: null,
+      availableNumber: 60,
+      hostAssociated: [
+        {
+          id: 0,
+          hostname: 'erclin6db18',
+          usedLicenses: 4
+        },
+        {
+          id: 0,
+          hostname: 'erclin7dbx',
+          usedLicenses: 8
+        }
+      ]
+    }
+  ],
   agreementParts: {}
 })
 
@@ -22,7 +73,7 @@ export const getters = {
 
 export const mutations = {
   SET_LICENSE_AGREEMENT: (state, payload) => {
-    state.licensesAgreement = payload
+    state.licensesAgreement.push(payload)
   },
   SET_AGREEMENT_PARTS: (state, payload) => {
     state.agreementParts = payload
@@ -34,7 +85,9 @@ export const actions = {
     const agreementList = await axiosDefault.get('/agreements/oracle/database')
     const response = await agreementList.data
 
-    commit('SET_LICENSE_AGREEMENT', response)
+    if (response.length > 0) {
+      commit('SET_LICENSE_AGREEMENT', response)
+    }
   },
   async getAgreementParts({ commit }) {
     const agreementParts = await axiosDefault.get(
