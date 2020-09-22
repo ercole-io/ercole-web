18b31d0f261a91330e2bbd2e14734e291dd557c6
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2020-09-22 12:03:44 -0300

    Agreement parts select is working

--- src/store/modules/licensesAgreement.js
+++ src/store/modules/licensesAgreement.js
@@ -1,21 +1,47 @@
+import axiosDefault from '../../axios/axios-default.js'
+import _ from 'lodash'
+
 export const state = () => ({
-  licensesAgreement: []
+  licensesAgreement: {},
+  agreementParts: {}
 })
 
 export const getters = {
-  // getterValue: state => {
-  //   return state.value
-  // }
+  returnAgreementParts: state => {
+    const agreeParts = []
+
+    _.map(state.agreementParts, val => {
+      agreeParts.push({
+        agreeParts: `${val.partID} - ${val.itemDescription} - ${val.metrics}`
+      })
+    })
+
+    return agreeParts
+  }
 }
 
 export const mutations = {
   SET_LICENSE_AGREEMENT: (state, payload) => {
-    state.licensesAgreement.push(payload)
+    state.licensesAgreement = payload
+  },
+  SET_AGREEMENT_PARTS: (state, payload) => {
+    state.agreementParts = payload
   }
 }
 
 export const actions = {
-  // updateActionValue({ commit }) {
-  //   commit('updateValue', payload)
-  // }
+  async getLicensesAgreement({ commit }) {
+    const agreementList = await axiosDefault.get('/agreements/oracle/database')
+    const response = await agreementList.data
+
+    commit('SET_LICENSE_AGREEMENT', response)
+  },
+  async getAgreementParts({ commit }) {
+    const agreementParts = await axiosDefault.get(
+      '/settings/oracle/database/agreements-parts'
+    )
+    const response = await agreementParts.data
+
+    commit('SET_AGREEMENT_PARTS', response)
+  }
 }
