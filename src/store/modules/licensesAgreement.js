c9e54eee069eb95a98c5da0fc279683470cc0349
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2020-09-24 09:09:38 -0300

    Add and remove license agreement

--- src/store/modules/licensesAgreement.js
+++ src/store/modules/licensesAgreement.js
@@ -2,62 +2,14 @@ import axiosDefault from '../../axios/axios-default.js'
 import _ from 'lodash'
 
 export const state = () => ({
-  licensesAgreement: [
-    {
-      id: 0,
-      agreeNumber: 5051863,
-      partNumber: 'L75978',
-      description: 'Oracle GoldenGate',
-      metrics: 'Processor Perpetual',
-      csi: 19765174,
-      referenceNumber: 103246681,
-      ula: 'no',
-      licenseNumber: 72,
-      userNumber: null,
-      availableNumber: 60,
-      hostAssociated: [
-        {
-          id: 0,
-          hostname: 'erclin6db18',
-          usedLicenses: 4
-        },
-        {
-          id: 0,
-          hostname: 'erclin7dbx',
-          usedLicenses: 8
-        }
-      ]
-    },
-    {
-      id: 2,
-      agreeNumber: 5051863,
-      partNumber: 'L75978',
-      description: 'Oracle GoldenGate',
-      metrics: 'Processor Perpetual',
-      csi: 19765174,
-      referenceNumber: 103246681,
-      ula: 'no',
-      licenseNumber: 72,
-      userNumber: null,
-      availableNumber: 60,
-      hostAssociated: [
-        {
-          id: 0,
-          hostname: 'erclin6db18',
-          usedLicenses: 4
-        },
-        {
-          id: 0,
-          hostname: 'erclin7dbx',
-          usedLicenses: 8
-        }
-      ]
-    }
-  ],
+  licensesAgreement: [],
   agreementParts: {}
 })
 
 export const getters = {
+  returnLicensesAgreement: state => {
+    return state.licensesAgreement
+  },
   returnAgreementParts: state => {
     const agreeParts = []
 
@@ -73,7 +25,7 @@ export const getters = {
 
 export const mutations = {
   SET_LICENSE_AGREEMENT: (state, payload) => {
-    state.licensesAgreement.push(payload)
+    state.licensesAgreement = payload
   },
   SET_AGREEMENT_PARTS: (state, payload) => {
     state.agreementParts = payload
@@ -85,9 +37,7 @@ export const actions = {
     const agreementList = await axiosDefault.get('/agreements/oracle/database')
     const response = await agreementList.data
 
-    if (response.length > 0) {
-      commit('SET_LICENSE_AGREEMENT', response)
-    }
+    commit('SET_LICENSE_AGREEMENT', response)
   },
   async getAgreementParts({ commit }) {
     const agreementParts = await axiosDefault.get(
