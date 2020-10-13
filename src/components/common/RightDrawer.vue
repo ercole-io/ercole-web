5c9cd2e871c76c504bd8972680bc1b2d987e3553
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2020-10-13 17:51:11 -0300

    Creating a right drawer component

---
+++ src/components/common/RightDrawer.vue
@@ -0,0 +1,61 @@
+<template>
+  <b-sidebar
+    type="is-white"
+    :fullheight="true"
+    :fullwidth="false"
+    :overlay="false"
+    :right="true"
+    position="absolute"
+    v-model="isOpenFilters"
+  >
+    <div class="drawer-wrapper has-background-light">
+      <b-button
+        size="is-small"
+        type="is-dark"
+        inverted
+        icon-pack="fas"
+        icon-right="times"
+        class="close"
+        @click="closeFilters"
+      />
+
+      <slot name="drawer-content" />
+    </div>
+  </b-sidebar>
+</template>
+
+<script>
+export default {
+  props: {
+    openFilters: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    closeFilters() {
+      this.$emit('closeFilters', false)
+    }
+  },
+  computed: {
+    isOpenFilters() {
+      return this.openFilters
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.drawer-wrapper {
+  padding: 100px 20px 60px 20px;
+  border-left: 3px solid #679189;
+  height: 100%;
+  position: relative;
+
+  .close {
+    position: absolute;
+    top: 90px;
+    right: 5px;
+  }
+}
+</style>
