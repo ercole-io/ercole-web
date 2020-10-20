b5bdfddcf356e72c0d12ada1cf213662fc3625f6
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2020-10-20 13:20:44 -0300

    Setting up the filters on licenses used

--- src/views/licenses/LicensesUsed.vue
+++ src/views/licenses/LicensesUsed.vue
@@ -1,5 +1,84 @@
 <template>
   <section>
+    <DrawerRight>
+      <BoxContent
+        slot="drawer-content"
+        title="Licenses Used Filters"
+        class="mt-5"
+      >
+        <form @submit.prevent="applyFilters">
+          <b-field label="Hostname" custom-class="is-small">
+            <b-autocomplete
+              v-model="licensesUsedFilters.hostname"
+              size="is-small"
+              type="number"
+              clearable
+              :data="filteredHostnames"
+              @typing="getAutocompleteData($event, 'hostname')"
+            >
+              <template slot="empty">No results found</template>
+            </b-autocomplete>
+          </b-field>
+
+          <b-field label="DB Name" custom-class="is-small">
+            <b-autocomplete
+              v-model="licensesUsedFilters.dbName"
+              size="is-small"
+              type="number"
+              clearable
+              :data="filteredDbnames"
+              @typing="getAutocompleteData($event, 'dbName')"
+            >
+              <template slot="empty">No results found</template>
+            </b-autocomplete>
+          </b-field>
+
+          <b-field label="License Name" custom-class="is-small">
+            <b-autocomplete
+              v-model="licensesUsedFilters.licenseName"
+              size="is-small"
+              type="number"
+              clearable
+              :data="filteredLicenseName"
+              @typing="getAutocompleteData($event, 'licenseName')"
+            >
+              <template slot="empty">No results found</template>
+            </b-autocomplete>
+          </b-field>
+
+          <b-field label="Used Licenses" custom-class="is-small">
+            <b-autocomplete
+              class="mr-1"
+              v-model="usedLicensesMin"
+              size="is-small"
+              :data="filteredUsedLicenses"
+              @typing="getAutocompleteData($event, 'usedLicenses')"
+            >
+            </b-autocomplete>
+            <b-autocomplete
+              class="ml-1"
+              v-model="usedLicensesMax"
+              size="is-small"
+              :data="filteredUsedLicenses"
+              @typing="getAutocompleteData($event, 'usedLicenses')"
+            >
+            </b-autocomplete>
+          </b-field>
+
+          <div
+            class="buttons is-flex mt-5"
+            style="justify-content: space-between;"
+          >
+            <b-button type="is-danger" size="is-small" @click="resetFilters">
+              Reset
+            </b-button>
+            <b-button type="is-primary" size="is-small" native-type="submit">
+              Apply
+            </b-button>
+          </div>
+        </form>
+      </BoxContent>
+    </DrawerRight>
     <BoxContent>
       <FullTable
         placeholder="Search on Licenses"
@@ -7,6 +86,8 @@
         :tableData="getUsedLicenses"
         :clickedRow="() => []"
       >
+        <DrawerButton slot="customTopHeader" tooltipText="More Filters" />
+
         <template slot="headData">
           <v-th sortKey="hostname">Hostname</v-th>
           <v-th sortKey="dbName">DB Name</v-th>
@@ -33,12 +114,19 @@
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
+import {
+  organizeKeysBeforeFilter,
+  returnAutocompleteData,
+  prepareDataForAutocomplete
+} from '@/helpers/helpers.js'
 import paginationMixin from '@/mixins/paginationMixin.js'
 import BoxContent from '@/components/common/BoxContent.vue'
 import FullTable from '@/components/common/Table/FullTable.vue'
 import exportButton from '@/components/common/exportButton.vue'
 import TdContent from '@/components/common/Table/TdContent.vue'
 import HostLink from '@/components/common/Table/HostLink.vue'
+import DrawerButton from '@/components/common/DrawerButton.vue'
+import DrawerRight from '@/components/common/DrawerRight.vue'
 
 export default {
   mixins: [paginationMixin],
@@ -47,23 +135,101 @@ export default {
     FullTable,
     exportButton,
     TdContent,
-    HostLink
+    HostLink,
+    DrawerButton,
+    DrawerRight
   },
   data() {
     return {
-      keys: ['hostname', 'dbName', 'licenseName', 'usedLicenses']
-      // data: []
+      keys: ['hostname', 'dbName', 'licenseName', 'usedLicenses'],
+      usedLicensesMin: null,
+      usedLicensesMax: null,
+      licensesUsedFilters: {},
+      filteredHostnames: [],
+      filteredDbnames: [],
+      filteredLicenseName: [],
+      filteredUsedLicenses: []
     }
   },
   async beforeMount() {
     await this.getLicensesList()
-    // this.data = this.licenses.licenseList
+
+    this.filteredHostnames = prepareDataForAutocomplete(
+      this.getUsedLicenses,
+      'hostname'
+    )
+
+    this.filteredDbnames = prepareDataForAutocomplete(
+      this.getUsedLicenses,
+      'dbName'
+    )
+
+    this.filteredLicenseName = prepareDataForAutocomplete(
+      this.getUsedLicenses,
+      'licenseName'
+    )
+
+    this.filteredUsedLicenses = prepareDataForAutocomplete(
+      this.getUsedLicenses,
+      'usedLicenses'
+    )
   },
   methods: {
-    ...mapActions(['getLicensesList'])
+    ...mapActions(['getLicensesList']),
+    applyFilters() {
+      if (this.usedLicensesMax) {
+        this.licensesUsedFilters.usedLicenses = [
+          Number(this.usedLicensesMin),
+          Number(this.usedLicensesMax)
+        ]
+      } else {
+        this.licensesUsedFilters.usedLicenses = Number(this.usedLicensesMin)
+      }
+
+      this.$store.commit('SET_FILTERS', {
+        status: true,
+        filters: organizeKeysBeforeFilter(this.licensesUsedFilters)
+      })
+    },
+    resetFilters() {
+      this.usedLicensesMin = null
+      this.usedLicensesMax = null
+      this.licensesUsedFilters = {}
+      this.$store.commit('SET_FILTERS', {
+        status: false,
+        filters: []
+      })
+    },
+    getAutocompleteData(text, toFilter) {
+      const autocomplete = returnAutocompleteData(
+        text,
+        this.getUsedLicenses,
+        toFilter
+      )
+
+      switch (toFilter) {
+        case 'hostname':
+          this.filteredHostnames = autocomplete
+          break
+        case 'dbName':
+          this.filteredDbnames = autocomplete
+          break
+        case 'licenseName':
+          this.filteredLicenseName = autocomplete
+          break
+        case 'usedLicenses':
+          this.filteredUsedLicenses = autocomplete
+          break
+        default:
+          break
+      }
+    }
   },
   computed: {
     ...mapGetters(['getUsedLicenses'])
+  },
+  beforeDestroy() {
+    this.resetFilters()
   }
 }
 </script>
