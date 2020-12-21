5e536765c8805e4347dc8033031f046e9e107fe8
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2020-12-21 15:58:15 -0300

    Highlighting searched term on tables

--- src/components/common/Table/TdArray.vue
+++ src/components/common/Table/TdArray.vue
@@ -1,14 +1,15 @@
 <template>
   <td v-tooltip="options(formatArrayToShow(value))">
-    {{ formatArrayToShow(value) }}
+    <span v-html="highlight(formatArrayToShow(value))"></span>
   </td>
 </template>
 
 <script>
+import HighlightSearchMixin from '@/mixins/highlightSearch.js'
 import TooltipMixin from '@/mixins/tooltipMixin.js'
 
 export default {
-  mixins: [TooltipMixin],
+  mixins: [HighlightSearchMixin, TooltipMixin],
   methods: {
     formatArrayToShow(val) {
       return val.toString().replace(/,\s*/g, '\n')
