e460324c4e910220b19819f7b5f409fa0674ea7b
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2021-01-11 15:59:47 -0300

    Fixing active db on click

--- src/mixins/hostnameLink.js
+++ src/mixins/hostnameLink.js
@@ -2,10 +2,17 @@ export default {
   methods: {
     handleHostnameClick() {
       if (this.$store.getters.checkHostnameExists(this.getHostname)) {
-        this.$router.push({
-          name: 'hosts-details',
-          params: { hostname: this.getHostname, dbname: this.getDbname }
-        })
+        if (this.getDbname !== '') {
+          this.$router.push({
+            name: 'hosts-details',
+            params: { hostname: this.getHostname, dbname: this.getDbname }
+          })
+        } else {
+          this.$router.push({
+            name: 'hosts-details',
+            params: { hostname: this.getHostname }
+          })
+        }
       } else {
         this.$router.push({ name: '404' })
       }
