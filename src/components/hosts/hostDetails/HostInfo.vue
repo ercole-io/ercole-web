ebc76e0dca8addae227abced476b69e5b2df8087
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2021-03-09 10:12:22 -0300

    Possibility to choose how many dbs to compare with total daily cpu usage; General refactoring on host details components

--- src/components/hosts/hostDetails/HostInfo.vue
+++ src/components/hosts/hostDetails/HostInfo.vue
@@ -1,24 +1,9 @@
 <template>
   <BoxContent>
     <div class="columns">
-      <div class="column">
-        <Card cardTitle="General info">
-          <CardList :list="getHostDetailInfo.general" />
-        </Card>
-      </div>
-      <div class="column">
-        <Card cardTitle="Agent">
-          <CardList :list="getHostDetailInfo.agent" />
-        </Card>
-      </div>
-      <div class="column">
-        <Card cardTitle="Virtual">
-          <CardList :list="getHostDetailInfo.virtual" />
-        </Card>
-      </div>
-      <div class="column">
-        <Card cardTitle="CPU">
-          <CardList :list="getHostDetailInfo.cpu" />
+      <div class="column" v-for="(info, i) in getHostDetailInfo" :key="i">
+        <Card :cardTitle="info.name">
+          <CardList :list="info.data" />
         </Card>
       </div>
     </div>
--- src/views/hosts/HostsDetails.vue
+++ src/views/hosts/HostsDetails.vue
@@ -1,58 +1,38 @@
 <template>
   <section v-if="isMounted">
-    <boxContent>
-      <div class="columns">
-        <div class="column is-9">
-          <NotificationsInfo />
-        </div>
-        <div
-          class="column is-3 has-text-right is-flex"
-          style="justify-content: flex-end;"
-        >
-          <Filesys />
-
-          <b-button
-            class="ml-2"
-            type="is-danger is-small"
-            @click="deleteHost(hostname)"
-          >
-            Dismiss
-          </b-button>
-        </div>
+    <div class="columns">
+      <div class="column is-8">
+        <NotificationsInfo />
       </div>
-    </boxContent>
+      <div class="column is-2 has-text-right">
+        <Filesys />
+      </div>
+      <div class="column is-2 has-text-right">
+        <DismissHost :hostname="hostname" />
+      </div>
+    </div>
 
-    <!-- <boxContent>
-      <HostTags
-        title="Host Tags:"
-        inputWidth="150"
-        :tagsList="['Tag 1', 'Tag 2', 'Tag 3', 'Tag 4']"
-        @addTag="handleAddTag"
-        @removeTag="handleRemoveTag"
-        @editTag="handleEditTag"
-      />
-    </boxContent> -->
+    <!-- <HostTags /> -->
 
     <HostInfo />
 
     <div class="columns">
-      <HostDatabases :activeDB="dbname" :hostname="hostname" />
-      <ChartCpu :chartData="chartData" :hostname="hostname" />
+      <Databases :activeDB="dbname" />
+      <ChartCpu />
     </div>
   </section>
 </template>
 
 <script>
 import { bus } from '@/helpers/eventBus.js'
-import { mapGetters, mapActions, mapState } from 'vuex'
-import BoxContent from '@/components/common/BoxContent.vue'
+import { mapActions } from 'vuex'
 import NotificationsInfo from '@/components/hosts/hostDetails/NotificationsInfo.vue'
-// import HostTags from '@/components/common/Tags.vue'
+// import HostTags from '@/components/hosts/hostDetails/HostTags.vue'
 import HostInfo from '@/components/hosts/hostDetails/HostInfo.vue'
 import ChartCpu from '@/components/hosts/hostDetails/ChartCpu.vue'
-import HostDatabases from '@/components/hosts/hostDetails/databases/Databases.vue'
+import Databases from '@/components/hosts/hostDetails/databases/Databases.vue'
 import Filesys from '@/components/hosts/hostDetails/Filesys.vue'
-import axiosDefault from '@/axios/axios-default.js'
+import DismissHost from '@/components/hosts/hostDetails/DismissHost.vue'
 
 export default {
   props: {
@@ -68,18 +48,16 @@ export default {
     }
   },
   components: {
-    BoxContent,
     NotificationsInfo,
     // HostTags,
     HostInfo,
     ChartCpu,
-    HostDatabases,
-
-    Filesys
+    Databases,
+    Filesys,
+    DismissHost
   },
   data() {
     return {
-      chartData: [],
       isMounted: false
     }
   },
@@ -87,49 +65,10 @@ export default {
     await this.getHostByName(this.hostname)
     bus.$emit('dynamicTitle', this.hostname)
 
-    this.chartData = this.getGpuGrowthChart
-
     this.isMounted = true
   },
   methods: {
-    ...mapActions(['getHostByName']),
-    deleteHost(hostname) {
-      this.$buefy.dialog.confirm({
-        title: 'Dismissing Host',
-        message: `Are you sure you want to <b>dismiss</b> the host <b>${hostname}</b>? This action cannot be undone.`,
-        confirmText: 'Dismiss Host',
-        type: 'is-danger',
-        hasIcon: true,
-        onConfirm: () => {
-          axiosDefault
-            .delete(`/hosts/${hostname}`)
-            .then(() => {
-              this.$router.push({ name: 'hosts' })
-            })
-            .then(() => {
-              this.$buefy.toast.open({
-                message: `The host ${hostname} was successfully dismissed!`,
-                type: 'is-success',
-                duration: 10000000000,
-                position: 'is-bottom'
-              })
-            })
-        }
-      })
-    },
-    handleAddTag(value) {
-      console.log('make api call to add tag', value)
-    },
-    handleRemoveTag(value) {
-      console.log('make api call to remove tag', value)
-    },
-    handleEditTag(value) {
-      console.log('make api call to edit tag', value)
-    }
-  },
-  computed: {
-    ...mapState(['hostDetails']),
-    ...mapGetters(['getCurrentHost', 'getGpuGrowthChart'])
+    ...mapActions(['getHostByName'])
   }
 }
 </script>
