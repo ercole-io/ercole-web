5ae3e710e16fc5fce373f7e88bc05e8b9ff65d2d
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2021-03-26 22:30:10 +0100

    #273 - updating host table

---
+++ src/components/common/ButtonGroup.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="is-flex is-justify-content-center is-align-items-center mb-5">
+    <span class="is-size-7 has-text-weight-medium mr-2" v-if="groupTitle">
+      {{ groupTitle }}
+    </span>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['groupTitle']
+}
+</script>
+
+<style lang="scss" scoped></style>
