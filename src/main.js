822b57a4d88043a5ee4fcb0f6fdd68e84dd936b6
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2021-03-30 18:13:31 +0200

    Adding more colors to chart colors array

--- src/main.js
+++ src/main.js
@@ -36,14 +36,17 @@ Vue.use(Buefy)
 Vue.use(Chartkick.use(Chart))
 Chartkick.options = {
   colors: [
-    'rgb(0, 63, 92)',
-    'rgb(243, 114, 44)',
-    'rgb(144, 190, 109)',
-    'rgb(248, 150, 30)',
-    'rgb(249, 199, 79)',
-    'rgb(249, 65, 68)',
-    'rgb(67, 170, 139)',
-    'rgb(87, 117, 144)'
+    'rgb(176, 101, 156)', // light purple
+    'rgb(254, 188, 59)', // light orange
+    'rgb(144, 190, 109)', // light green
+    'rgb(253, 128, 128)', // light red
+    'rgb(71, 172, 177)', // light blue
+    'rgb(232, 221, 9)', // yellow
+    'rgb(248, 150, 30)', // dark orange
+    'rgb(139, 117, 215)', // dark purple
+    'rgb(249, 65, 68)', // dark red
+    'rgb(67, 170, 139)', // dark green
+    'rgb(87, 117, 144)' // dark blue
   ].reverse()
 }
 
