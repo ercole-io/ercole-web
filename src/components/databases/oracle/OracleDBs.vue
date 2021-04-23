99a9cef5b35e8d982cad3a3886a92f0ae9062c8b
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2021-04-23 17:03:43 +0200

    Reorganizing some columns to show data when the button is clicked like hosts page

--- src/components/databases/oracle/OracleDBs.vue
+++ src/components/databases/oracle/OracleDBs.vue
@@ -1,55 +1,128 @@
 <template>
-  <FullTable
-    placeholder="Search on Oracle DBs"
-    :keys="keys"
-    :tableData="getAllOracleDBs"
-    @clickedRow="handleClickedRow"
-    isClickable
-  >
-    <template slot="headData">
-      <v-th sortKey="name">DB Name</v-th>
-      <v-th sortKey="version">DB Version</v-th>
-      <v-th sortKey="hostname">Hostname</v-th>
-      <v-th sortKey="environment">Env.</v-th>
-      <v-th sortKey="charset">Charset</v-th>
-      <v-th sortKey="work">Work</v-th>
-      <v-th sortKey="cpuCount">CPU Count</v-th>
-      <v-th sortKey="blockSize">Block Size</v-th>
-      <v-th sortKey="status">Status</v-th>
-      <v-th sortKey="uniqueName">Unique Name</v-th>
-      <v-th sortKey="memory">Memory (GB)</v-th>
-      <v-th sortKey="datafileSize">Datafile size (GB)</v-th>
-      <v-th sortKey="segmentsSize">Segment size (GB)</v-th>
-      <v-th sortKey="archivelog">Archivelog Mode</v-th>
-      <v-th sortKey="dataguard">DR</v-th>
-      <v-th sortKey="ha">HA</v-th>
-    </template>
+  <section>
+    <ButtonGroup
+      groupTitle="Show more info:"
+      hAlign="is-justify-content-flex-start"
+    >
+      <b-button
+        class="mr-1"
+        size="is-small"
+        :type="hideReliability ? 'is-light' : 'is-light reliability'"
+        @click="hideReliability = !hideReliability"
+      >
+        Reliability
+      </b-button>
+      <b-button
+        class="mr-1"
+        size="is-small"
+        :type="hideSpaceUsed ? 'is-light' : 'is-light spaceUsed'"
+        @click="hideSpaceUsed = !hideSpaceUsed"
+      >
+        Space Used
+      </b-button>
+      <b-button
+        class="mr-1"
+        size="is-small"
+        :type="hideCharset ? 'is-light' : 'is-light charset'"
+        @click="hideCharset = !hideCharset"
+      >
+        Charset
+      </b-button>
+    </ButtonGroup>
+    <FullTable
+      placeholder="Search on Oracle DBs"
+      :keys="keys"
+      :tableData="getAllOracleDBs"
+      @clickedRow="handleClickedRow"
+      isClickable
+    >
+      <template slot="headData">
+        <v-th sortKey="name">DB Name</v-th>
+        <v-th
+          sortKey="archivelog"
+          class="reliability"
+          :class="{ hide: hideReliability }"
+          >Archivelog Mode</v-th
+        >
+        <v-th
+          sortKey="dataguard"
+          class="reliability"
+          :class="{ hide: hideReliability }"
+          >DR</v-th
+        >
+        <v-th
+          sortKey="ha"
+          class="reliability"
+          :class="{ hide: hideReliability }"
+          >HA</v-th
+        >
+        <v-th
+          sortKey="datafileSize"
+          class="spaceUsed"
+          :class="{ hide: hideSpaceUsed }"
+          >Datafile size (GB)</v-th
+        >
+        <v-th
+          sortKey="segmentsSize"
+          class="spaceUsed"
+          :class="{ hide: hideSpaceUsed }"
+          >Segment size (GB)</v-th
+        >
+        <v-th sortKey="charset" class="charset" :class="{ hide: hideCharset }"
+          >Charset</v-th
+        >
+        <v-th sortKey="version">DB Version</v-th>
+        <v-th sortKey="hostname">Hostname</v-th>
+        <v-th sortKey="environment">Env.</v-th>
+        <v-th sortKey="work">Work</v-th>
+        <v-th sortKey="cpuCount">CPU Count</v-th>
+        <v-th sortKey="blockSize">Block Size</v-th>
+        <v-th sortKey="status">Status</v-th>
+        <v-th sortKey="uniqueName">Unique Name</v-th>
+        <v-th sortKey="memory">Memory (GB)</v-th>
+      </template>
 
-    <template slot="bodyData" slot-scope="rowData">
-      <TdContent :value="rowData.scope.name" />
-      <TdContent :value="rowData.scope.version" />
-      <HostLink :hostname="[rowData.scope.hostname, rowData.scope.name]" />
-      <TdContent :value="rowData.scope.environment" />
-      <TdContent :value="rowData.scope.charset" />
-      <TdContent :value="rowData.scope.work | formatNumber('0.00')" />
-      <TdContent :value="rowData.scope.cpuCount | formatNumber('0.00')" />
-      <TdContent :value="rowData.scope.blockSize" />
-      <TdContent :value="rowData.scope.status" />
-      <TdContent :value="rowData.scope.uniqueName" />
-      <TdContent :value="rowData.scope.memory | formatNumber('0.00')" />
-      <TdContent :value="rowData.scope.datafileSize" />
-      <TdContent :value="rowData.scope.segmentsSize" />
-      <TdIcon :value="rowData.scope.archivelog" />
-      <TdIcon :value="rowData.scope.dataguard" />
-      <TdIcon :value="rowData.scope.ha" />
-    </template>
+      <template slot="bodyData" slot-scope="rowData">
+        <TdContent :value="rowData.scope.name" />
+        <TdIcon
+          :value="rowData.scope.archivelog"
+          :class="{ hide: hideReliability }"
+        />
+        <TdIcon
+          :value="rowData.scope.dataguard"
+          :class="{ hide: hideReliability }"
+        />
+        <TdIcon :value="rowData.scope.ha" :class="{ hide: hideReliability }" />
+        <TdContent
+          :value="rowData.scope.datafileSize"
+          :class="{ hide: hideSpaceUsed }"
+        />
+        <TdContent
+          :value="rowData.scope.segmentsSize"
+          :class="{ hide: hideSpaceUsed }"
+        />
+        <TdContent
+          :value="rowData.scope.charset"
+          :class="{ hide: hideCharset }"
+        />
+        <TdContent :value="rowData.scope.version" />
+        <HostLink :hostname="[rowData.scope.hostname, rowData.scope.name]" />
+        <TdContent :value="rowData.scope.environment" />
+        <TdContent :value="rowData.scope.work | formatNumber('0.00')" />
+        <TdContent :value="rowData.scope.cpuCount | formatNumber('0.00')" />
+        <TdContent :value="rowData.scope.blockSize" />
+        <TdContent :value="rowData.scope.status" />
+        <TdContent :value="rowData.scope.uniqueName" />
+        <TdContent :value="rowData.scope.memory | formatNumber('0.00')" />
+      </template>
 
-    <ExportButton
-      slot="export"
-      url="hosts/technologies/oracle/databases"
-      expName="oracleDbs"
-    />
-  </FullTable>
+      <ExportButton
+        slot="export"
+        url="hosts/technologies/oracle/databases"
+        expName="oracleDbs"
+      />
+    </FullTable>
+  </section>
 </template>
 
 <script>
@@ -60,6 +133,7 @@ import ExportButton from '@/components/common/exportButton.vue'
 import TdContent from '@/components/common/Table/TdContent.vue'
 import TdIcon from '@/components/common/Table/TDIcon.vue'
 import HostLink from '@/components/common/Table/HostLink.vue'
+import ButtonGroup from '@/components/common/ButtonGroup.vue'
 
 export default {
   mixins: [hostnameLinkRow],
@@ -68,7 +142,8 @@ export default {
     ExportButton,
     TdContent,
     TdIcon,
-    HostLink
+    HostLink,
+    ButtonGroup
   },
   data() {
     return {
@@ -89,7 +164,10 @@ export default {
         'cpuCount',
         'status',
         'uniqueName'
-      ]
+      ],
+      hideReliability: true,
+      hideSpaceUsed: true,
+      hideCharset: true
     }
   },
   computed: {
@@ -98,4 +176,18 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.hide {
+  display: none;
+}
+
+.reliability {
+  box-shadow: inset 0 -14px 0 -10px #f37021 !important;
+}
+.spaceUsed {
+  box-shadow: inset 0 -14px 0 -10px #fcd217 !important;
+}
+.charset {
+  box-shadow: inset 0 -14px 0 -10px #9c4d1e !important;
+}
+</style>
