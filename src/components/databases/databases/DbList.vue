612e587b93d217dcbaca695abd46886f3596f567
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2021-05-03 15:25:46 +0200

    Inserting a show/hide reliability columns on databases page

--- src/components/databases/databases/DbList.vue
+++ src/components/databases/databases/DbList.vue
@@ -1,47 +1,86 @@
 <template>
-  <FullTable
-    placeholder="Search on Databases"
-    :keys="keys"
-    :tableData="getAllDatabases"
-    @clickedRow="handleClickedRow"
-    isClickable
-  >
-    <template slot="headData">
-      <v-th sortKey="name">DB Name</v-th>
-      <v-th sortKey="type">DB Type</v-th>
-      <v-th sortKey="version">DB Version</v-th>
-      <v-th sortKey="hostname">Hostname</v-th>
-      <v-th sortKey="environment">Env.</v-th>
-      <v-th sortKey="charset">Charset</v-th>
-      <v-th sortKey="memory">Memory (GB)</v-th>
-      <v-th sortKey="datafileSize">Datafile Size (GB)</v-th>
-      <v-th sortKey="segmentSize">Segment Size (GB)</v-th>
-      <v-th sortKey="archivelog">Archivelog Mode</v-th>
-      <v-th sortKey="disasterRecovery">DR</v-th>
-      <v-th sortKey="highAvailability">HA</v-th>
-    </template>
+  <section>
+    <ButtonGroup
+      groupTitle="Show more info:"
+      hAlign="is-justify-content-flex-start"
+    >
+      <b-button
+        class="mr-1"
+        size="is-small"
+        :type="hideReliability ? 'is-light' : 'is-light reliability'"
+        @click="hideReliability = !hideReliability"
+      >
+        Reliability
+      </b-button>
+    </ButtonGroup>
+    <FullTable
+      placeholder="Search on Databases"
+      :keys="keys"
+      :tableData="getAllDatabases"
+      @clickedRow="handleClickedRow"
+      isClickable
+    >
+      <template slot="headData">
+        <v-th sortKey="name">DB Name</v-th>
+        <v-th
+          sortKey="archivelog"
+          class="reliability"
+          :class="{ hide: hideReliability }"
+          >Archivelog Mode</v-th
+        >
+        <v-th
+          sortKey="dataguard"
+          class="reliability"
+          :class="{ hide: hideReliability }"
+          >DR</v-th
+        >
+        <v-th
+          sortKey="ha"
+          class="reliability"
+          :class="{ hide: hideReliability }"
+          >HA</v-th
+        >
+        <v-th sortKey="type">DB Type</v-th>
+        <v-th sortKey="version">DB Version</v-th>
+        <v-th sortKey="hostname">Hostname</v-th>
+        <v-th sortKey="environment">Env.</v-th>
+        <v-th sortKey="charset">Charset</v-th>
+        <v-th sortKey="memory">Memory (GB)</v-th>
+        <v-th sortKey="datafileSize">Datafile Size (GB)</v-th>
+        <v-th sortKey="segmentSize">Segment Size (GB)</v-th>
+      </template>
 
-    <template slot="bodyData" slot-scope="rowData">
-      <TdContent :value="rowData.scope.name" />
-      <TdContent :value="rowData.scope.type" />
-      <TdContent :value="rowData.scope.version" />
-      <HostLink :hostname="[rowData.scope.hostname, rowData.scope.name]" />
-      <TdContent :value="rowData.scope.environment" />
-      <TdContent :value="rowData.scope.charset" />
-      <TdContent :value="rowData.scope.memory | formatNumber('0.00')" />
-      <TdContent :value="rowData.scope.datafileSize | formatNumber('0.00')" />
-      <TdContent :value="rowData.scope.segmentSize | formatNumber('0.00')" />
-      <TdIcon :value="rowData.scope.archivelog" />
-      <TdIcon :value="rowData.scope.disasterRecovery" />
-      <TdIcon :value="rowData.scope.highAvailability" />
-    </template>
+      <template slot="bodyData" slot-scope="rowData">
+        <TdContent :value="rowData.scope.name" />
+        <TdIcon
+          :value="rowData.scope.archivelog"
+          :class="{ hide: hideReliability }"
+        />
+        <TdIcon
+          :value="rowData.scope.disasterRecovery"
+          :class="{ hide: hideReliability }"
+        />
+        <TdIcon
+          :value="rowData.scope.highAvailability"
+          :class="{ hide: hideReliability }"
+        />
+        <TdContent :value="rowData.scope.type" />
+        <TdContent :value="rowData.scope.version" />
+        <HostLink :hostname="[rowData.scope.hostname, rowData.scope.name]" />
+        <TdContent :value="rowData.scope.environment" />
+        <TdContent :value="rowData.scope.charset" />
+        <TdContent :value="rowData.scope.memory | formatNumber('0.00')" />
+        <TdContent :value="rowData.scope.datafileSize | formatNumber('0.00')" />
+        <TdContent :value="rowData.scope.segmentSize | formatNumber('0.00')" />
+      </template>
 
-    <ExportButton
-      slot="export"
-      url="hosts/technologies/oracle/databases"
-      expName="databases"
-    />
-  </FullTable>
+      <ExportButton
+        slot="export"
+        url="hosts/technologies/oracle/databases"
+        expName="databases"
+      />
+    </FullTable>
+  </section>
 </template>
 
 <script>
@@ -52,6 +91,7 @@ import ExportButton from '@/components/common/exportButton.vue'
 import TdContent from '@/components/common/Table/TdContent.vue'
 import TdIcon from '@/components/common/Table/TDIcon.vue'
 import HostLink from '@/components/common/Table/HostLink.vue'
+import ButtonGroup from '@/components/common/ButtonGroup.vue'
 
 export default {
   mixins: [hostnameLinkRow],
@@ -60,7 +100,8 @@ export default {
     ExportButton,
     TdContent,
     TdIcon,
-    HostLink
+    HostLink,
+    ButtonGroup
   },
   data() {
     return {
@@ -77,7 +118,8 @@ export default {
         'archivelog',
         'disasterRecovery',
         'highAvailability'
-      ]
+      ],
+      hideReliability: true
     }
   },
   computed: {
@@ -86,4 +128,12 @@ export default {
 }
 </script>
 
-<style lang="sass" scoped></style>
+<style lang="scss" scoped>
+.hide {
+  display: none;
+}
+
+.reliability {
+  box-shadow: inset 0 -14px 0 -10px #f37021 !important;
+}
+</style>
