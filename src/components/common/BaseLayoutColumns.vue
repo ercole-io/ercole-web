056926f7b43fad265a53ed7d4e97ea4ab0685941
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2021-05-05 21:39:07 +0200

    Creating a base columns dynamic layout

---
+++ src/components/common/BaseLayoutColumns.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="columns">
+    <div
+      v-for="(col, i) in pageCols"
+      :key="i"
+      class="column"
+      :class="`is-${col.size}`"
+    >
+      <slot :name="col.name" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pageCols: {
+      type: Array,
+      default: () => [
+        { size: '3', name: 'col1' },
+        { size: '9', name: 'col2' }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
