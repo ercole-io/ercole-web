b37e0347660d9ecb56c95edcdec352d5728869f9
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2021-05-05 20:54:39 +0200

    Upgrading alerts page to the new advanced filters

--- src/components/alerts/AlertsFilters.vue
+++ src/components/alerts/AlertsFilters.vue
@@ -1,110 +1,106 @@
 <template>
-  <DrawerFilters title="Alerts Filters">
-    <form @submit.prevent="apply">
-      <CustomField label="Status">
-        <b-select
-          v-model="alertStatus"
-          size="is-small"
-          placeholder="Select a Status"
-          expanded
-        >
-          <option value="NEW">NEW</option>
-          <option value="ACK">ACK</option>
-          <option value="">All</option>
-        </b-select>
-      </CustomField>
+  <AdvancedFiltersBase filterTitle="Alerts Filters" :submitAction="apply">
+    <CustomField label="Status">
+      <b-select
+        v-model="alertStatus"
+        size="is-small"
+        placeholder="Select a Status"
+        expanded
+      >
+        <option value="NEW">NEW</option>
+        <option value="ACK">ACK</option>
+        <option value="">All</option>
+      </b-select>
+    </CustomField>
 
-      <CustomField label="Type">
-        <b-select
-          v-model="filters.alertCategory"
-          size="is-small"
-          placeholder="Select a Type"
-          expanded
-        >
-          <option :value="null" v-if="filters.alertCategory">
-            Reset
-          </option>
-          <option value="AGENT">AGENT</option>
-          <option value="ENGINE">ENGINE</option>
-          <option value="LICENSE">LICENSE</option>
-        </b-select>
-      </CustomField>
+    <CustomField label="Type">
+      <b-select
+        v-model="filters.alertCategory"
+        size="is-small"
+        placeholder="Select a Type"
+        expanded
+      >
+        <option :value="null" v-if="filters.alertCategory">
+          Reset
+        </option>
+        <option value="AGENT">AGENT</option>
+        <option value="ENGINE">ENGINE</option>
+        <option value="LICENSE">LICENSE</option>
+      </b-select>
+    </CustomField>
 
-      <CustomField label="Date">
-        <b-datepicker
-          v-model="startDate"
-          size="is-small"
-          placeholder="Start Date"
-          position="is-bottom-right"
-          icon="calendar-today"
-          :max-date="endDate ? endDate : new Date()"
-          :date-formatter="formatDate"
-          class="mr-1"
-          trap-focus
-        />
-        <b-datepicker
-          v-model="endDate"
-          size="is-small"
-          placeholder="End Date"
-          position="is-bottom-left"
-          icon="calendar-today"
-          :min-date="startDate"
-          :max-date="new Date()"
-          :date-formatter="formatDate"
-          class="ml-1"
-          trap-focus
-        />
-      </CustomField>
+    <CustomField label="Date">
+      <b-datepicker
+        v-model="startDate"
+        size="is-small"
+        placeholder="Start Date"
+        position="is-bottom-right"
+        icon="calendar-today"
+        :max-date="endDate ? endDate : new Date()"
+        :date-formatter="formatDate"
+        class="mr-1"
+        trap-focus
+      />
+      <b-datepicker
+        v-model="endDate"
+        size="is-small"
+        placeholder="End Date"
+        position="is-bottom-left"
+        icon="calendar-today"
+        :min-date="startDate"
+        :max-date="new Date()"
+        :date-formatter="formatDate"
+        class="ml-1"
+        trap-focus
+      />
+    </CustomField>
 
-      <CustomField label="Severity">
-        <b-select
-          v-model="filters.alertSeverity"
-          size="is-small"
-          placeholder="Select a Severity"
-          expanded
-        >
-          <option :value="null" v-if="filters.alertSeverity">
-            Reset
-          </option>
-          <option value="INFO">INFO</option>
-          <option value="WARNING">WARNING</option>
-          <option value="CRITICAL">CRITICAL</option>
-        </b-select>
-      </CustomField>
+    <CustomField label="Severity">
+      <b-select
+        v-model="filters.alertSeverity"
+        size="is-small"
+        placeholder="Select a Severity"
+        expanded
+      >
+        <option :value="null" v-if="filters.alertSeverity">
+          Reset
+        </option>
+        <option value="INFO">INFO</option>
+        <option value="WARNING">WARNING</option>
+        <option value="CRITICAL">CRITICAL</option>
+      </b-select>
+    </CustomField>
 
-      <CustomField label="Hostname">
-        <b-autocomplete
-          v-model="filters.hostname"
-          size="is-small"
-          clearable
-          :data="filteredData"
-          @typing="setFilteredAutocomplete($event, 'hostname', getAlerts)"
-        />
-      </CustomField>
+    <CustomField label="Hostname">
+      <b-autocomplete
+        v-model="filters.hostname"
+        size="is-small"
+        clearable
+        :data="filteredData"
+        @typing="setFilteredAutocomplete($event, 'hostname', getAlerts)"
+      />
+    </CustomField>
 
-      <CustomField label="Code">
-        <b-autocomplete
-          v-model="filters.alertCode"
-          size="is-small"
-          clearable
-          :data="filteredData"
-          @typing="setFilteredAutocomplete($event, 'alertCode', getAlerts)"
-        />
-      </CustomField>
+    <CustomField label="Code">
+      <b-autocomplete
+        v-model="filters.alertCode"
+        size="is-small"
+        clearable
+        :data="filteredData"
+        @typing="setFilteredAutocomplete($event, 'alertCode', getAlerts)"
+      />
+    </CustomField>
 
-      <CustomField label="Description">
-        <b-autocomplete
-          v-model="filters.description"
-          size="is-small"
-          clearable
-          :data="filteredData"
-          @typing="setFilteredAutocomplete($event, 'description', getAlerts)"
-        />
-      </CustomField>
-
-      <ActionButtons />
-    </form>
-  </DrawerFilters>
+    <CustomField label="Description">
+      <b-autocomplete
+        v-model="filters.description"
+        size="is-small"
+        clearable
+        :data="filteredData"
+        @typing="setFilteredAutocomplete($event, 'description', getAlerts)"
+      />
+    </CustomField>
+  </AdvancedFiltersBase>
 </template>
 
 <script>
@@ -112,16 +108,14 @@ import { bus } from '@/helpers/eventBus.js'
 import { formatDatepickerDate } from '@/helpers/helpers.js'
 import { mapGetters, mapActions, mapState } from 'vuex'
 import localFiltersMixin from '@/mixins/localFiltersMixin.js'
-import DrawerFilters from '@/components/common/DrawerFilters.vue'
-import ActionButtons from '@/components/common/Form/ActionButtons.vue'
+import AdvancedFiltersBase from '@/components/common/AdvancedFiltersBase.vue'
 import CustomField from '@/components/common/Form/CustomField.vue'
 import formatDate from '@/filters/formatDate.js'
 
 export default {
   mixins: [localFiltersMixin],
   components: {
-    DrawerFilters,
-    ActionButtons,
+    AdvancedFiltersBase,
     CustomField
   },
   data() {
