690607971b1ad9ed8adea1fd3d69132379015aae
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2021-05-14 16:36:28 +0200

    Revert "Fixing some bugs at mysql agreements"
    This reverts commit e51c3cc5177b797006f56a380ab80e495b1e93fa.

--- src/components/hosts/hostDetails/databases/oracle/OracleDatabases.vue
+++ src/components/hosts/hostDetails/databases/oracle/OracleDatabases.vue
@@ -1,15 +1,20 @@
 <template>
-  <b-tabs size="is-small" type="is-boxed" :animated="true" v-model="isActive">
-    <!-- @click.native="changeChart" -->
+  <b-tabs
+    size="is-small"
+    type="is-boxed"
+    :animated="true"
+    v-model="isActive"
+    @click.native="changeChart"
+  >
     <template v-for="(dbs, i) in currentDBs">
       <b-tab-item :key="i" :label="dbs.name">
         <b-tabs size="is-small" type="is-toggle" vertical :animated="true">
-          <DbInfo :dbInfo="dbs" />
+          <!-- <DbInfo :dbInfo="dbs" /> -->
           <!-- <DbTags
               :dbName="dbs.name"
               :tags="['Albino', 'Milano', 'Como', 'Torino']"
             /> -->
-          <DbPDBs :pdbs="dbs.pdbs" />
+          <!-- <DbPDBs :pdbs="dbs.pdbs" />
           <DbLicenses :licenses="dbs.licenses" />
           <DbOptions :options="dbs.featureUsageStats" />
           <DbTablespaces :tablespaces="dbs.tablespaces" />
@@ -20,7 +25,7 @@
           <DbSegmentAdvisors :segmentAdvisors="dbs.segmentAdvisors" />
           <DbGrowth :growth="dbs.changes" :growthId="dbs.name" />
           <DbBackups :backups="dbs.backups" />
-          <DbServices :services="dbs.services" />
+          <DbServices :services="dbs.services" /> -->
         </b-tabs>
       </b-tab-item>
     </template>
@@ -28,68 +33,61 @@
 </template>
 
 <script>
-// import { bus } from '@/helpers/eventBus.js'
-import _ from 'lodash'
-import { mapGetters } from 'vuex'
-import DbInfo from '@/components/hosts/hostDetails/databases/oracle/DbInfo.vue'
-import DbTablespaces from '@/components/hosts/hostDetails/databases/oracle/DbTablespaces.vue'
-import DbSchemas from '@/components/hosts/hostDetails/databases/oracle/DbSchemas.vue'
-import DbPatches from '@/components/hosts/hostDetails/databases/oracle/DbPatches.vue'
-import DbPSUs from '@/components/hosts/hostDetails/databases/oracle/DbPSUs.vue'
-import DbADDMs from '@/components/hosts/hostDetails/databases/oracle/DbADDMs.vue'
-import DbSegmentAdvisors from '@/components/hosts/hostDetails/databases/oracle/DbSegmentAdvisors.vue'
-import DbBackups from '@/components/hosts/hostDetails/databases/oracle/DbBackups.vue'
-import DbServices from '@/components/hosts/hostDetails/databases/oracle/DbServices.vue'
-import DbGrowth from '@/components/hosts/hostDetails/databases/oracle/DbGrowth.vue'
-import DbOptions from '@/components/hosts/hostDetails/databases/oracle/DbOptions.vue'
-import DbLicenses from '@/components/hosts/hostDetails/databases/oracle/DbLicenses.vue'
+import { bus } from '@/helpers/eventBus.js'
+// import DbInfo from '@/components/hosts/hostDetails/databases/oracle/DbInfo.vue'
+// import DbTablespaces from '@/components/hosts/hostDetails/databases/oracle/DbTablespaces.vue'
+// import DbSchemas from '@/components/hosts/hostDetails/databases/oracle/DbSchemas.vue'
+// import DbPatches from '@/components/hosts/hostDetails/databases/oracle/DbPatches.vue'
+// import DbPSUs from '@/components/hosts/hostDetails/databases/oracle/DbPSUs.vue'
+// import DbADDMs from '@/components/hosts/hostDetails/databases/oracle/DbADDMs.vue'
+// import DbSegmentAdvisors from '@/components/hosts/hostDetails/databases/oracle/DbSegmentAdvisors.vue'
+// import DbBackups from '@/components/hosts/hostDetails/databases/oracle/DbBackups.vue'
+// import DbServices from '@/components/hosts/hostDetails/databases/oracle/DbServices.vue'
+// import DbGrowth from '@/components/hosts/hostDetails/databases/oracle/DbGrowth.vue'
+// import DbOptions from '@/components/hosts/hostDetails/databases/oracle/DbOptions.vue'
+// import DbLicenses from '@/components/hosts/hostDetails/databases/oracle/DbLicenses.vue'
 // import DbTags from '@/components/hosts/hostDetails/databases/oracle/DbTags.vue'
-import DbPDBs from '@/components/hosts/hostDetails/databases/oracle/DbPDBs.vue'
+// import DbPDBs from '@/components/hosts/hostDetails/databases/oracle/DbPDBs.vue'
 
 export default {
   props: {
     currentDBs: {
       type: Array,
       default: () => []
+    },
+    activatedTab: {
+      type: Number,
+      default: 0
     }
     // changeChart: {
     //   type: Function
     // }
   },
   components: {
-    DbInfo,
-    DbTablespaces,
-    DbSchemas,
-    DbPatches,
-    DbPSUs,
-    DbADDMs,
-    DbSegmentAdvisors,
-    DbBackups,
-    DbServices,
-    DbGrowth,
-    DbOptions,
-    DbLicenses,
+    // DbInfo,
+    // DbTablespaces,
+    // DbSchemas,
+    // DbPatches,
+    // DbPSUs,
+    // DbADDMs,
+    // DbSegmentAdvisors,
+    // DbBackups,
+    // DbServices,
+    // DbGrowth,
+    // DbOptions,
+    // DbLicenses,
     // DbTags,
-    DbPDBs
-  },
-  data() {
-    return {
-      activeTab: 0,
-      searchDb: ''
-    }
-  },
-  beforeMount() {
-    this.findActiveTab()
-  },
-  methods: {
-    findActiveTab() {
-      this.activeTab = _.findIndex(this.currentHostFiltered(this.searchDb), {
-        name: this.currentHostActiveDB
-      })
-    }
+    // DbPDBs
   },
   computed: {
-    ...mapGetters(['currentHostFiltered'])
+    isActive: {
+      get() {
+        return this.activatedTab
+      },
+      set(val) {
+        return bus.$emit('changeActiveTab', val)
+      }
+    }
   }
 }
 </script>
--- src/filters/toUpper.js
+++
@@ -1,4 +0,0 @@
-export default value => {
-  if (!value) return ''
-  return value.toString().toUpperCase()
-}
