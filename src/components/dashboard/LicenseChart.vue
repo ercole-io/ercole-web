d27a9577e8b3737a24ae8c045aa6ece3fc23271c
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2021-05-18 22:15:20 +0200

    Range dates for licenses chart on dashboard

--- src/components/dashboard/LicenseChart.vue
+++ src/components/dashboard/LicenseChart.vue
@@ -20,29 +20,77 @@
         </b-select>
       </b-field>
     </div>
-    <LineChart chartId="lincenseChart" :lineChartData="finalChartData" />
+    <div class="range-dates">
+      <b-datepicker
+        v-model="startDate"
+        size="is-small"
+        placeholder="Start Date"
+        position="is-bottom-right"
+        icon="calendar-today"
+        :max-date="endDate ? endDate : new Date()"
+        :date-formatter="formatDate"
+        class="mr-1 range-dates-field"
+        trap-focus
+      />
+      <b-datepicker
+        v-model="endDate"
+        size="is-small"
+        placeholder="End Date"
+        position="is-bottom-left"
+        icon="calendar-today"
+        :min-date="startDate"
+        :max-date="new Date()"
+        :date-formatter="formatDate"
+        class="ml-1 range-dates-field"
+        trap-focus
+      />
+      <b-button
+        class="ml-1"
+        size="is-small"
+        type="is-primary"
+        icon-right="delete"
+        @click="getToday"
+      />
+    </div>
+    <LineChart
+      chartId="lincenseChart"
+      :lineChartData="finalChartData"
+      v-if="showChart"
+    />
+    <NoContent
+      v-if="!showChart"
+      :noContentText="'Please change the dates!'"
+      style="min-height: 200px"
+    />
   </BoxContent>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
 import _ from 'lodash'
 import moment from 'moment'
+import { mapGetters } from 'vuex'
+import formatDate from '@/filters/formatDate.js'
 import BoxContent from '@/components/common/BoxContent.vue'
 import LineChart from '@/components/common/charts/LineChart.vue'
+import NoContent from '@/components/common/NoContent.vue'
 
 const matchType = (data, selected) => {
   return _.find(data, type => {
     return type.licenseTypeID === selected
   })
 }
-const mapLicenseType = (history, type) => {
+const mapLicenseType = (history, type, dateRange) => {
   const historyByType = []
+
   _.map(history, value => {
-    historyByType.push({
-      date: moment(value.date).format('YYYY-MM-DD'),
-      value: type === 'purchased' ? value.covered : value.consumed
-    })
+    let date = moment(value.date).format('YYYY-MM-DD')
+
+    if (date > dateRange[0] && date < dateRange[1]) {
+      historyByType.push({
+        date: date,
+        value: type === 'purchased' ? value.covered : value.consumed
+      })
+    }
   })
 
   return historyByType
@@ -73,39 +121,85 @@ const buildFinalData = (purchased, used) => {
 export default {
   components: {
     LineChart,
-    BoxContent
+    BoxContent,
+    NoContent
   },
   data() {
     return {
       finalChartData: [],
-      selectedType: ''
+      selectedType: '',
+      startDate: null,
+      endDate: null,
+      showChart: false
     }
   },
   async beforeMount() {
     await this.$store.dispatch('getLicenseHistory')
     this.selectedType = this.getChartLicenseHistory[0].licenseTypeID
+
+    this.getToday()
+
     this.mountLincenseChart()
   },
   methods: {
     mountLincenseChart() {
       let findType = matchType(this.getChartLicenseHistory, this.selectedType)
+      const dateRange = [
+        moment(this.startDate).format('YYYY-MM-DD'),
+        moment(this.endDate).format('YYYY-MM-DD')
+      ]
 
-      const purchasedLicenses = mapLicenseType(findType.history, 'purchased')
-      const usedLicenses = mapLicenseType(findType.history, 'used')
+      const purchased = mapLicenseType(findType.history, 'purchased', dateRange)
+      const used = mapLicenseType(findType.history, 'used', dateRange)
 
-      const resultPurchased = loopLicenseTypeValues(purchasedLicenses)
-      const resultUsed = loopLicenseTypeValues(usedLicenses)
+      const resultPurchased = loopLicenseTypeValues(purchased)
+      const resultUsed = loopLicenseTypeValues(used)
 
       const finalData = buildFinalData(resultPurchased, resultUsed)
 
       this.finalChartData = finalData
-      return this.finalChartData
+    },
+    getToday() {
+      const today = moment(new Date(), 'YYYY/MM/DD')
+      this.startDate = new Date(moment().format('YYYY-MM-01'))
+      this.endDate = new Date(moment().format(`YYYY-MM-${today.format('DD')}`))
+    },
+    formatDate(date) {
+      return formatDate(date)
     }
   },
   computed: {
     ...mapGetters(['getChartLicenseHistory'])
+  },
+  watch: {
+    finalChartData(values) {
+      values.length > 0 ? (this.showChart = true) : (this.showChart = false)
+    },
+    startDate(newValue) {
+      if (newValue) {
+        this.mountLincenseChart()
+      }
+    },
+    endDate(newValue) {
+      if (newValue) {
+        this.mountLincenseChart()
+      }
+    }
   }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.range-dates {
+  display: flex;
+  justify-content: flex-end;
+  flex-direction: row;
+  margin-right: -3px;
+  margin-top: -10px;
+
+  .range-dates-field {
+    width: 100%;
+    max-width: 180px;
+  }
+}
+</style>
