cac8eb3468a787a45511e836a9039e05eea41f25
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2021-05-28 18:23:49 +0200

    Creating customs and reusable form elements

---
+++ src/components/common/Form/CustomDatepicker.vue
@@ -0,0 +1,43 @@
+<template>
+  <b-datepicker
+    v-model="dateVal"
+    size="is-small"
+    :placeholder="placeholder"
+    position="is-bottom-right"
+    icon="calendar-today"
+    :max-date="new Date()"
+    :date-formatter="formatDate"
+    class="mr-1"
+  />
+</template>
+
+<script>
+import formatDate from '@/filters/formatDate.js'
+
+export default {
+  props: {
+    value: {},
+    placeholder: {
+      type: String,
+      default: 'Set a Date'
+    }
+  },
+  methods: {
+    formatDate(date) {
+      return formatDate(date)
+    }
+  },
+  computed: {
+    dateVal: {
+      get() {
+        return this.value
+      },
+      set(val) {
+        this.$emit('input', val)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
