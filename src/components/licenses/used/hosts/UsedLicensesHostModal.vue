fa84ca5cd9109412b725c3a03580b6f9654b0200
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2021-06-16 16:42:40 +0200

    Modal to show the licenses used databases per host

---
+++ src/components/licenses/used/hosts/UsedLicensesHostModal.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="modal-card" style="width: 500px;">
+    <b-loading
+      :is-full-page="false"
+      v-model="isLoading"
+      :can-cancel="false"
+    ></b-loading>
+    <header class="modal-card-head">
+      <p class="modal-card-title">
+        List of Databases
+        <br />
+        <span class="is-size-7">Hostname: {{ hostname }}</span>
+        <br />
+        <span class="is-size-7">License ID: {{ licenseId }}</span>
+      </p>
+    </header>
+    <section class="modal-card-body">
+      <FullTable
+        placeholder="Search on Hosts Associated"
+        :keys="keys"
+        :tableData="databases"
+        :clickedRow="() => []"
+      >
+        <template slot="headData">
+          <v-th sortKey="dbName" style="width: 100%">Database Name</v-th>
+        </template>
+
+        <template slot="bodyData" slot-scope="rowData">
+          <TdContent :value="rowData.scope.dbName" />
+        </template>
+      </FullTable>
+    </section>
+    <footer class="modal-card-foot"></footer>
+  </div>
+</template>
+
+<script>
+import TooltipMixin from '@/mixins/tooltipMixin.js'
+import FullTable from '@/components/common/Table/FullTable.vue'
+import TdContent from '@/components/common/Table/TdContent.vue'
+
+export default {
+  mixins: [TooltipMixin],
+  components: {
+    FullTable,
+    TdContent
+  },
+  props: {
+    databases: {
+      type: [Array, Object],
+      required: true
+    },
+    licenseId: {},
+    hostname: {}
+  },
+  data() {
+    return {
+      keys: ['dbName'],
+      isLoading: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.modal-card-title {
+  font-size: 1.2rem;
+}
+
+.modal-card-body {
+  font-size: 1rem;
+  cursor: default;
+}
+</style>
