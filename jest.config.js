8d0de6d2aca09ca3fc579830a435f4e13943c4b7
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2021-07-01 19:57:55 +0200

    Fix unit tests for i18n

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
-  collectCoverage: true,
+  collectCoverage: false,
   collectCoverageFrom: [
     'src/**/*.{js,vue}',
     '!src/main.js' // No need to cover bootstrap file
-  ]
+  ],
+  verbose: true
 }
