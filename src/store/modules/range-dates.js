632c185ebf2a5eb4923d897154fda98f5bf3bb48
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2021-07-13 21:38:35 +0200

    Inserting range dates on db growth charts

--- src/store/modules/range-dates.js
+++ src/store/modules/range-dates.js
@@ -1,12 +1,23 @@
 import moment from 'moment'
 
 export const state = () => ({
-  rangeDates: []
+  rangeDates: [],
+  rangeDatesAlt: []
 })
 
 export const mutations = {
   SET_RANGE_DATES: (state, payload) => {
     state.rangeDates = [
+      moment(payload[0])
+        .subtract(1, 'days')
+        .format('YYYY-MM-DD'),
+      moment(payload[1])
+        .add(1, 'days')
+        .format('YYYY-MM-DD')
+    ]
+  },
+  SET_RANGE_DATES_ALT: (state, payload) => {
+    state.rangeDatesAlt = [
       moment(payload[0])
         .subtract(1, 'days')
         .format('YYYY-MM-DD'),
