e721b0bf5c0e3738f7916f59bb8b04c7d437e9ec
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2021-07-16 19:13:51 +0200

    Creating a dynamic heading component

---
+++ src/components/common/Table/DynamicHeading.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-th
+    :sortKey="data.sort"
+    :class="
+      data.group && moreInfoToggle['hidden' + data.group]
+        ? 'is-hidden'
+        : data.class
+    "
+  >
+    {{ $t(`${data.langKey}`) }}
+  </v-th>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  props: ['data'],
+  computed: {
+    ...mapState(['moreInfoToggle'])
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
