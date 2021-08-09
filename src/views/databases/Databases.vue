9272fdff530e4252bd6081f1168232e5de95d9e9
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2021-08-09 19:19:04 +0200

    Updating databases page with the new toggle columns component

--- src/views/databases/Databases.vue
+++ src/views/databases/Databases.vue
@@ -1,39 +1,14 @@
 <template>
-  <BaseLayoutColumns
-    v-if="isMounted"
-    :pageCols="[
-      { colsize: '9', slotName: 'content' },
-      { colsize: '3', slotName: 'side' }
-    ]"
-  >
-    <DbList slot="content" />
-    <div slot="side">
-      <DbTotalMemorySize class="mb-4" />
-      <DbTotalSegmentSize class="mb-4" />
-      <DbCharts
-        id="databasesChart"
-        chartHeight="500px"
-        :xAxesConfig="[true, 'top']"
-      />
-    </div>
-  </BaseLayoutColumns>
+  <DbList v-if="isMounted" />
 </template>
 
 <script>
 import { mapActions } from 'vuex'
-import BaseLayoutColumns from '@/components/common/BaseLayoutColumns.vue'
 import DbList from '@/components/databases/databases/DbList.vue'
-import DbCharts from '@/components/databases/databases/DbCharts.vue'
-import DbTotalMemorySize from '@/components/databases/databases/DbTotalMemorySize.vue'
-import DbTotalSegmentSize from '@/components/databases/databases/DbTotalSegmentSize.vue'
 
 export default {
   components: {
-    BaseLayoutColumns,
-    DbList,
-    DbCharts,
-    DbTotalMemorySize,
-    DbTotalSegmentSize
+    DbList
   },
   data() {
     return {
