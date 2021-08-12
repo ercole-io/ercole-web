e845c028d6f45a407f2e90c535f0276d9a2364e0
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2021-08-12 18:04:34 +0200

    Setting up hosts core chart on dashboard

---
+++ src/components/dashboard/CoreHostsChart.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <RangeDates
+      :setRange="SET_RANGE_DATES_ALT"
+      totalRange="30"
+      class="mt-0 mb-4"
+    />
+    <LineChart
+      chartId="coreHostsChart"
+      :hasLegend="false"
+      :lineChartData="getChartCoreHosts"
+    />
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters, mapMutations } from 'vuex'
+import LineChart from '@/components/common/charts/LineChart.vue'
+import RangeDates from '@/components/common/RangeDates.vue'
+
+export default {
+  components: {
+    LineChart,
+    RangeDates
+  },
+  async beforeMount() {
+    await this.getCoreHosts()
+
+    this.getChartCoreHosts
+  },
+  methods: {
+    ...mapActions(['getCoreHosts']),
+    ...mapMutations(['SET_RANGE_DATES_ALT'])
+  },
+  computed: {
+    ...mapGetters(['getChartCoreHosts'])
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
