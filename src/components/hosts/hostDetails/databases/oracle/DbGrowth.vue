e85d08393bc2e1a5e542b15599ffd0c465abea85
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2021-08-12 18:25:37 +0200

    Updating all line charts to use a common function from helpers

--- src/components/hosts/hostDetails/databases/oracle/DbGrowth.vue
+++ src/components/hosts/hostDetails/databases/oracle/DbGrowth.vue
@@ -2,7 +2,7 @@
   <b-tab-item label="DB Growth" v-if="growth.length > 0">
     <RangeDates
       :setRange="SET_RANGE_DATES_ALT"
-      totalRange="30"
+      totalRange="31"
       class="mt-0 mr-0"
     />
     <LineChart
@@ -17,7 +17,11 @@
 import _ from 'lodash'
 import moment from 'moment'
 import { mapMutations, mapState } from 'vuex'
-import { setRangeDateFormat, checkRangeDate } from '@/helpers/helpers.js'
+import {
+  setRangeDateFormat,
+  checkRangeDate,
+  getKeyValuePair
+} from '@/helpers/helpers.js'
 import LineChart from '@/components/common/charts/LineChart.vue'
 import RangeDates from '@/components/common/RangeDates.vue'
 
@@ -61,7 +65,7 @@ export default {
 
       _.reverse(datafile)
 
-      return this.mountFinalData(datafile)
+      return getKeyValuePair(datafile, 'date', 'value')
     },
     calcSegmentsSize() {
       const segments = []
@@ -80,7 +84,7 @@ export default {
 
       _.reverse(segments)
 
-      return this.mountFinalData(segments)
+      return getKeyValuePair(segments, 'date', 'value')
     },
     calcAllocatedSize() {
       const allocated = []
@@ -99,15 +103,7 @@ export default {
 
       _.reverse(allocated)
 
-      return this.mountFinalData(allocated)
-    },
-    mountFinalData(result) {
-      const finalResult = {}
-      for (const prop in result) {
-        finalResult[result[prop].date] = result[prop].value
-      }
-
-      return finalResult
+      return getKeyValuePair(allocated, 'date', 'value')
     }
   },
   computed: {
