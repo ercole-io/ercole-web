f884d9af3d16c187e0bb01021b699c55c8c338ce
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2021-08-16 16:54:01 +0200

    Organizing recommendations page into components and inserting toggle columns

---
+++ src/components/cloud/recommendations/RecommendationsList.vue
@@ -0,0 +1,58 @@
+<template>
+  <FullTable
+    slot="center"
+    placeholder="Cloud Recommendations"
+    :keys="getHeadKeys(getRecommendations)"
+    :tableData="getRecommendations"
+  >
+    <template slot="headData">
+      <v-th sortKey="name">
+        Recommendation
+      </v-th>
+      <v-th sortKey="type">
+        Cloud Infrastructure
+      </v-th>
+      <v-th sortKey="resourceCounts[0].count">
+        Pending
+      </v-th>
+      <v-th sortKey="estimatedCostSaving">
+        Estimated Savings
+      </v-th>
+      <v-th sortKey="lifecycleState">
+        Status
+      </v-th>
+      <v-th sortKey="importance">
+        Importance
+      </v-th>
+    </template>
+
+    <template slot="bodyData" slot-scope="rowData">
+      <TdContent :value="rowData.scope.name" />
+      <TdContent :value="rowData.scope.type" />
+      <TdContent :value="rowData.scope.resourceCounts[0].count" />
+      <TdContent :value="`€${rowData.scope.estimatedCostSaving}/month`" />
+      <TdContent :value="rowData.scope.lifecycleState" />
+      <TdContent :value="rowData.scope.importance" />
+    </template>
+  </FullTable>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import FullTable from '@/components/common/Table/FullTable.vue'
+import TdContent from '@/components/common/Table/TdContent.vue'
+import getHeadKeys from '@/mixins/dynamicHeadingMixin.js'
+
+export default {
+  mixins: [getHeadKeys],
+  components: {
+    FullTable,
+    TdContent
+  },
+  computed: {
+    ...mapGetters(['getRecommendations'])
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
