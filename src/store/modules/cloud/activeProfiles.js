5743ef60b1d34fedeae97869ded4faddccd9ccf9
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2021-10-26 17:43:05 +0200

    Recommendations messages and translations

--- src/store/modules/cloud/activeProfiles.js
+++ src/store/modules/cloud/activeProfiles.js
@@ -1,15 +1,25 @@
+import _ from 'lodash'
+
 export const state = () => ({
-  ociActiveProfile: []
+  ociActiveProfile: [],
+  ociActiveProfileError: ''
 })
 
 export const getters = {
   getOciActiveProfiles: state => {
     return state.ociActiveProfile
+  },
+  getOciActiveProfileError: state => {
+    const errNumber = _.slice(state.ociActiveProfileError)[0]
+    return errNumber ? errNumber : ''
   }
 }
 
 export const mutations = {
   SET_OCI_ACTIVE_PROFILE: (state, payload) => {
     state.ociActiveProfile = payload
+  },
+  SET_OCI_ACTIVE_PROFILE_ERROR: (state, payload) => {
+    state.ociActiveProfileError = payload
   }
 }
