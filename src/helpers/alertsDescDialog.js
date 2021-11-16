2e030c1a01708eb7286862605c102f270a03d8c9
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2021-11-16 16:25:53 +0100

    Fixing display description in alerts moda

--- src/helpers/alertsDescDialog.js
+++ src/helpers/alertsDescDialog.js
@@ -3,25 +3,22 @@ import { resolveSeverityIcon } from '@/helpers/helpers.js'
 import formatDateTime from '@/filters/formatDateTime.js'
 import i18n from '@/i18n.js'
 
-export const descriptionAlertDialog = data => {
+export const descriptionAlertDialog = (data) => {
   Dialog.alert({
     title: i18n.t('views.alerts.descModalTitle'),
     message: `
-      <b>${data.code}</b> 
-      </br> 
-      ${data.host} 
-      </br> 
-      ${data.categ} 
-      </br> 
-      ${formatDateTime(data.date)} 
-      </br></br> 
-      ${data.desc}
+      <p><b>${data.code}</b></p>
+      <p>${data.host}</p>
+      <p>${data.categ}</p>
+      <p>${formatDateTime(data.date)}</p>
+      <br>
+      <p style="white-space: break-spaces;">${data.desc}</p>
     `,
     confirmText: i18n.t('common.general.close'),
     size: 'is-small',
     hasIcon: true,
     iconPack: 'mdi',
     icon: resolveSeverityIcon(data.severity)[0],
-    type: resolveSeverityIcon(data.severity)[1]
+    type: resolveSeverityIcon(data.severity)[1],
   })
 }
