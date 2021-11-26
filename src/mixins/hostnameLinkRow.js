49677ad181804a23af6d4e4eafcb0ad344542cd6
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2021-11-26 17:52:11 +0100

    Removing host link from dismissed hosts on alerts page

--- src/mixins/hostnameLinkRow.js
+++ src/mixins/hostnameLinkRow.js
@@ -2,25 +2,29 @@ export default {
   methods: {
     handleClickedRow(value) {
       if (value.length > 0) {
-        if (this.$store.getters.checkHostnameExists(value[0].hostname)) {
-          if (value[0].name || value[0].dbname || value[0].dbName) {
-            this.$router.push({
-              name: 'hosts-details',
-              params: {
-                hostname: value[0].hostname,
-                dbname: value[0].name || value[0].dbname || value[0].dbName
-              }
-            })
+        if (value[0].alertStatus !== 'DISMISSED') {
+          if (this.$store.getters.checkHostnameExists(value[0].hostname)) {
+            if (value[0].name || value[0].dbname || value[0].dbName) {
+              this.$router.push({
+                name: 'hosts-details',
+                params: {
+                  hostname: value[0].hostname,
+                  dbname: value[0].name || value[0].dbname || value[0].dbName,
+                },
+              })
+            } else {
+              this.$router.push({
+                name: 'hosts-details',
+                params: { hostname: value[0].hostname },
+              })
+            }
           } else {
-            this.$router.push({
-              name: 'hosts-details',
-              params: { hostname: value[0].hostname }
-            })
+            this.$router.push({ name: '404' })
           }
-        } else {
-          this.$router.push({ name: '404' })
         }
+      } else {
+        return
       }
-    }
-  }
+    },
+  },
 }
