629cd11655263bc272c2e4cc238fa6fc47b1a5c7
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2022-03-10 16:15:33 +0100

    Updating ghost loading for advanced filters

--- src/components/common/MoreInfoButtons.vue
+++ src/components/common/MoreInfoButtons.vue
@@ -1,56 +1,69 @@
 <template>
   <ButtonGroup :groupTitle="`${$t('common.general.moreInfo')}`">
-    <b-button
-      v-for="(button, i) in buttonItems"
-      :key="i"
-      class="mr-1"
-      size="is-small"
-      :type="
-        toggle['hidden' + button.toggle]
-          ? 'is-light'
-          : `is-light color-${i + 1}`
-      "
-      @click="
-        toggleMoreInfo(
-          button.toggle,
-          (toggle['hidden' + button.toggle] = !toggle['hidden' + button.toggle])
-        )
-      "
-    >
-      {{ $t(`${button.langKey}`) }}
-    </b-button>
+    <template v-for="(button, i) in buttonItems">
+      <GhostLoading
+        :isLoading="loadingTableStatus"
+        setWidth="60"
+        setHeight="30"
+        class="mr-1"
+        :key="i"
+      >
+        <b-button
+          :key="i"
+          class="mr-1"
+          size="is-small"
+          :type="
+            toggle['hidden' + button.toggle]
+              ? 'is-light'
+              : `is-light color-${i + 1}`
+          "
+          @click="
+            toggleMoreInfo(
+              button.toggle,
+              (toggle['hidden' + button.toggle] =
+                !toggle['hidden' + button.toggle])
+            )
+          "
+        >
+          {{ $t(`${button.langKey}`) }}
+        </b-button>
+      </GhostLoading>
+    </template>
   </ButtonGroup>
 </template>
 
 <script>
-import { mapMutations, mapState } from 'vuex'
+import { mapGetters, mapMutations, mapState } from 'vuex'
 import ButtonGroup from '@/components/common/ButtonGroup.vue'
+import GhostLoading from '@/components/common/GhostLoading.vue'
 
 export default {
   props: {
     buttonItems: {
       type: Array,
-      required: true
-    }
+      required: true,
+    },
   },
   components: {
-    ButtonGroup
+    ButtonGroup,
+    GhostLoading,
   },
   methods: {
     ...mapMutations(['SET_VISIBLE_COLS']),
     toggleMoreInfo(name, value) {
       this.SET_VISIBLE_COLS({
         value: value,
-        name: name
+        name: name,
       })
-    }
+    },
   },
   computed: {
     ...mapState(['moreInfoToggle']),
+    ...mapGetters(['loadingTableStatus']),
     toggle() {
       return this.moreInfoToggle
-    }
-  }
+    },
+  },
 }
 </script>
 
