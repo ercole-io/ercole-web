8e8823075294b00f0fdb73a3c7d243d3c6a41497
Mohab Abugabal <mabugabal@sorint.it>
2022-03-14 10:28:47 +0100

    ADDM table view enhancement

--- src/views/databases/oracle/ADDM.vue
+++ src/views/databases/oracle/ADDM.vue
@@ -14,6 +14,7 @@
       @clickedRow="handleClickedRow"
       isClickable
       :isLoadingTable="loadingTableStatus"
+      class="ADDM-table-view"
     >
       <template slot="headData">
         <v-th sortKey="benefit" defaultSort="desc">{{
