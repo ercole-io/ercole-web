4c1fdc700eae1c4dcf52b5037770acb7f1ae74d2
Mohab Abugabal <mabugabal@sorint.it>
2022-03-15 15:29:30 +0100

    embedded scss to component

--- src/views/databases/oracle/ADDM.vue
+++ src/views/databases/oracle/ADDM.vue
@@ -14,7 +14,6 @@
       @clickedRow="handleClickedRow"
       isClickable
       :isLoadingTable="loadingTableStatus"
-      class="ADDM-table-view"
     >
       <template slot="headData">
         <v-th sortKey="benefit" defaultSort="desc">{{
@@ -30,12 +29,12 @@
       </template>
 
       <template slot="bodyData" slot-scope="rowData">
-        <TdContent :value="rowData.scope.benefit" />
+        <TdContent :value="rowData.scope.benefit" class="findingColumn" />
         <HostLink :hostname="[rowData.scope.hostname, rowData.scope.dbname]" />
         <TdContent :value="rowData.scope.dbname" />
-        <TdContent :value="rowData.scope.finding" />
+        <TdContent :value="rowData.scope.finding"/>
         <TdContent :value="rowData.scope.recommendation" />
-        <TdContent :value="rowData.scope.action" />
+        <TdContent :value="rowData.scope.action" class="actionColumn" />
       </template>
 
       <ExportButton
@@ -92,4 +91,11 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.findingColumn{
+  width: 10%;
+}
+.actionColumn{
+  width: 50%;
+}
+</style>
