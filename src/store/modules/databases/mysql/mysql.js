2681b3d15f5724c096ccf017afae43ae085810c9
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2022-03-16 21:38:37 +0100

    Table loading for mysql page

--- src/store/modules/databases/mysql/mysql.js
+++ src/store/modules/databases/mysql/mysql.js
@@ -1,34 +1,39 @@
-import axiosDefault from '@/axios/axios-default.js'
+import axiosNoLoading from '@/axios/axios-no-loading.js'
 
 export const state = () => ({
-  mysqlDbs: []
+  mysqlDbs: [],
 })
 
 export const getters = {
   getAllMysqlDbs: (state, getters) => {
     return getters.filteredOrNot(state.mysqlDbs)
-  }
+  },
 }
 
 export const mutations = {
   SET_MYSQL_DBS: (state, payload) => {
     state.mysqlDbs = payload
-  }
+  },
 }
 
 export const actions = {
-  async getMysqlDbs({ commit, getters }) {
-    const mysqlDbs = await axiosDefault.get(
+  async getMysqlDbs({ commit, dispatch, getters }) {
+    dispatch('onLoadingTable')
+
+    const mysqlDbs = await axiosNoLoading.get(
       '/hosts/technologies/mysql/databases',
       {
         params: {
           'older-than': getters.getActiveFilters.date,
           environment: getters.getActiveFilters.environment,
-          location: getters.getActiveFilters.location
-        }
+          location: getters.getActiveFilters.location,
+        },
       }
     )
     const response = (await mysqlDbs.data.databases) || []
-    commit('SET_MYSQL_DBS', response)
-  }
+    if (response) {
+      dispatch('offLoadingTable')
+      commit('SET_MYSQL_DBS', response)
+    }
+  },
 }
