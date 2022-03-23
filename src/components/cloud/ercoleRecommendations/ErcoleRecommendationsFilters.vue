bd8b1221dfd644826c63cf748c640f1389b3cfe2
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2022-03-23 19:03:20 +0100

    Collapsible for OCI pages

--- src/components/cloud/ercoleRecommendations/ErcoleRecommendationsFilters.vue
+++ src/components/cloud/ercoleRecommendations/ErcoleRecommendationsFilters.vue
@@ -1,43 +1,52 @@
 <template>
   <AdvancedFiltersBase :submitAction="apply">
-    <CustomField :label="$t('common.fields.type')">
-      <CustomSelect v-model="filters.type" :options="filteredtype" />
-    </CustomField>
+    <Collapse :collapses="collapses">
+      <template slot="General">
+        <CustomField :label="$t('common.fields.type')">
+          <CustomSelect v-model="filters.type" :options="filteredtype" />
+        </CustomField>
 
-    <CustomField :label="$t('common.fields.compartmentName')">
-      <CustomAutocomplete
-        v-model="filters.compartmentName"
-        :filterResult="filteredcompartmentName"
-        :filterMethod="setAutocompletes"
-      />
-    </CustomField>
+        <CustomField :label="$t('common.fields.compartmentName')">
+          <CustomAutocomplete
+            v-model="filters.compartmentName"
+            :filterResult="filteredcompartmentName"
+            :filterMethod="setAutocompletes"
+          />
+        </CustomField>
 
-    <CustomField :label="$t('common.fields.repoName')">
-      <CustomAutocomplete
-        v-model="filters.name"
-        :filterResult="filteredname"
-        :filterMethod="setAutocompletes"
-      />
-    </CustomField>
+        <CustomField :label="$t('common.fields.repoName')">
+          <CustomAutocomplete
+            v-model="filters.name"
+            :filterResult="filteredname"
+            :filterMethod="setAutocompletes"
+          />
+        </CustomField>
 
-    <CustomField :label="$t('common.fields.resourceID')">
-      <CustomAutocomplete
-        v-model="filters.resourceID"
-        :filterResult="filteredresourceID"
-        :filterMethod="setAutocompletes"
-      />
-    </CustomField>
+        <CustomField :label="$t('common.fields.resourceID')">
+          <CustomAutocomplete
+            v-model="filters.resourceID"
+            :filterResult="filteredresourceID"
+            :filterMethod="setAutocompletes"
+          />
+        </CustomField>
+      </template>
+    </Collapse>
   </AdvancedFiltersBase>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 import localFiltersMixin from '@/mixins/localFiltersMixin.js'
+import Collapse from '@/components/common/Collapse.vue'
 
 export default {
   mixins: [localFiltersMixin],
+  components: {
+    Collapse,
+  },
   data() {
     return {
+      collapses: ['General'],
       autocompletes: ['name', 'compartmentName', 'resourceID'],
       selects: ['type'],
     }
