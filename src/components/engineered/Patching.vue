a82264e1366d9296ce2dfcba7afebdf1ec32d92c
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2022-03-31 21:43:53 +0200

    Updating  Engineered Systems loadings

--- src/components/engineered/Patching.vue
+++ src/components/engineered/Patching.vue
@@ -1,11 +1,13 @@
 <template>
   <BoxContent title="Patching" border :mbottom="false">
-    <BarChart
-      chartId="patchingChart"
-      :barChartData="getPatchingChartData"
-      chartHeight="110px"
-      stacked
-    />
+    <GhostLoading :isLoading="loadingTableStatus" setHeight="110">
+      <BarChart
+        chartId="patchingChart"
+        :barChartData="getPatchingChartData"
+        chartHeight="110px"
+        stacked
+      />
+    </GhostLoading>
   </BoxContent>
 </template>
 
@@ -13,16 +15,18 @@
 import { mapGetters, mapState } from 'vuex'
 import BoxContent from '@/components/common/BoxContent.vue'
 import BarChart from '@/components/common/charts/BarChart.vue'
+import GhostLoading from '@/components/common/GhostLoading.vue'
 
 export default {
   components: {
     BoxContent,
-    BarChart
+    BarChart,
+    GhostLoading,
   },
   computed: {
     ...mapState(['engineeredSystems']),
-    ...mapGetters(['getPatchingChartData'])
-  }
+    ...mapGetters(['getPatchingChartData', 'loadingTableStatus']),
+  },
 }
 </script>
 
