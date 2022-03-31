89c40b595b4b2f0da1af88c8397276df5890a250
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2022-03-31 20:18:11 +0200

    Updating error pages and crating a generic error page

--- src/helpers/errorHandler.js
+++ src/helpers/errorHandler.js
@@ -1,18 +1,22 @@
 import store from '../store/index.js'
 import router from '../router/index.js'
 
-const errorResponseHandler = error => {
+const errorResponseHandler = (error) => {
   if (error.config.errorHandle === false) {
     return Promise.reject(error)
   }
 
-  if (error.response) {
-    store.dispatch('offLoading')
-    if (error.response.status === 401) {
-      router.push('/401')
-    }
-    if (error.response.status === 500) {
-      router.push('/500')
+  if (error) {
+    if (error.response) {
+      store.dispatch('offLoading')
+      if (error.response.status === 401) {
+        router.push('/401')
+      }
+      if (error.response.status === 500) {
+        router.push('/500')
+      }
+    } else {
+      router.push('/error')
     }
   }
 }
