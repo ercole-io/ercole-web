2ae543c23ac5c74ec8d9d69e663f0c9db5e10c76
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2022-04-04 17:32:44 +0200

    Unblock view ms sqlserver databases in host details page

---
+++ src/components/hosts/hostDetails/databases/microsoft/MicrosoftDatabases.vue
@@ -0,0 +1,40 @@
+<template>
+  <b-tabs
+    size="is-small"
+    type="is-boxed"
+    :animated="true"
+    v-model="isActive"
+    @input="onChange"
+  >
+    <template v-for="(dbs, i) in currentDBs">
+      <b-tab-item :key="i" :label="dbs.name">
+        <b-tabs size="is-small" type="is-toggle" vertical :animated="true">
+          <DbInfo :dbInfo="dbs" />
+          <DbDatabases :databases="dbs.databases" />
+          <DbSegmantAdvisors :segmentAdvisors="dbs.segmentAdvisors" />
+          <DbTableSchemas :tableSchemas="dbs.tableSchemas" />
+        </b-tabs>
+      </b-tab-item>
+    </template>
+  </b-tabs>
+</template>
+
+<script>
+import hostDetailsDatabasesMixins from '@/mixins/hostDetailsDatabases.js'
+import DbInfo from '@/components/hosts/hostDetails/databases/mysql/DbInfo.vue'
+import DbDatabases from '@/components/hosts/hostDetails/databases/mysql/DbDatabases.vue'
+import DbSegmantAdvisors from '@/components/hosts/hostDetails/databases/mysql/DbSegmantAdvisors.vue'
+import DbTableSchemas from '@/components/hosts/hostDetails/databases/mysql/DbTableSchemas.vue'
+
+export default {
+  mixins: [hostDetailsDatabasesMixins],
+  components: {
+    DbInfo,
+    DbDatabases,
+    DbSegmantAdvisors,
+    DbTableSchemas,
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
