cc04d11aaccfe77a60ee91002e746f656a74959d
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2022-04-20 18:12:45 +0200

    Fix console errors for recommendations advanced filters

--- src/mixins/localFiltersMixin.js
+++ src/mixins/localFiltersMixin.js
@@ -87,6 +87,9 @@ export default {
       filteredimportance: [],
       filteredcompartmentName: [],
       filteredresourceID: [],
+      filteredcategory: [],
+      filteredobjectType: [],
+      filteredsuggestion: [],
 
       minmemorytotal: null,
       maxmemorytotal: null,
