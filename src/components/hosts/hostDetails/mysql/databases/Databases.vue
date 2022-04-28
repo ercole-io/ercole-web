639dccc316703a4871a7e8287051bfaf75ca1c17
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2022-04-28 20:35:56 +0200

    INserting licenses tab to mysql host details databases

--- src/components/hosts/hostDetails/mysql/databases/Databases.vue
+++ src/components/hosts/hostDetails/mysql/databases/Databases.vue
@@ -10,6 +10,7 @@
       <b-tab-item :key="i" :label="dbs.name">
         <b-tabs size="is-small" type="is-toggle" vertical :animated="true">
           <DbInfo :dbInfo="dbs" />
+          <DbLicenses :databases="dbs.dbLicenses" :dbName="dbs.name" />
           <DbDatabases :databases="dbs.databases" />
           <DbSegmantAdvisors :segmentAdvisors="dbs.segmentAdvisors" />
           <DbTableSchemas :tableSchemas="dbs.tableSchemas" />
@@ -25,6 +26,7 @@ import DbInfo from '@/components/hosts/hostDetails/mysql/databases/DbInfo.vue'
 import DbDatabases from '@/components/hosts/hostDetails/mysql/databases/DbDatabases.vue'
 import DbSegmantAdvisors from '@/components/hosts/hostDetails/mysql/databases/DbSegmantAdvisors.vue'
 import DbTableSchemas from '@/components/hosts/hostDetails/mysql/databases/DbTableSchemas.vue'
+import DbLicenses from '@/components/hosts/hostDetails/mysql/databases/DbLicenses.vue'
 
 export default {
   mixins: [hostDetailsDatabasesMixins],
@@ -33,6 +35,7 @@ export default {
     DbDatabases,
     DbSegmantAdvisors,
     DbTableSchemas,
+    DbLicenses,
   },
 }
 </script>
