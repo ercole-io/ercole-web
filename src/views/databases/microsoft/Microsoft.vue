3fe9a7794d5cbef910a49cedad8e003172773c5b
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2022-05-03 19:42:12 +0200

    Microsoft SqlServer page and menu with all default funcionalities

---
+++ src/views/databases/microsoft/Microsoft.vue
@@ -0,0 +1,29 @@
+<template>
+  <MsSqlServerDbs />
+</template>
+
+<script>
+import { bus } from '@/helpers/eventBus.js'
+import { mapActions, mapGetters } from 'vuex'
+import MsSqlServerDbs from '@/components/databases/microsoft/MsSqlServerDbs.vue'
+
+export default {
+  components: {
+    MsSqlServerDbs,
+  },
+  async beforeMount() {
+    await this.getMsSqlServerDbs().then(() => {
+      bus.$emit('data', this.getAllMsSqlServerDbs)
+    })
+  },
+
+  methods: {
+    ...mapActions(['getMsSqlServerDbs']),
+  },
+  computed: {
+    ...mapGetters(['getAllMsSqlServerDbs']),
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
