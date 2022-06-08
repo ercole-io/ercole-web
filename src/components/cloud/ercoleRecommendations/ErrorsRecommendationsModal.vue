997f6ce2a95c3249c0c7e060509b032c8fa9df44
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2022-06-08 19:12:42 +0200

    OCI Recommendations new api's #527

---
+++ src/components/cloud/ercoleRecommendations/ErrorsRecommendationsModal.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="modal-card animation-content" style="width: auto">
+    <header class="modal-card-head">
+      <b class="modal-card-title">Recommendations Errors</b>
+    </header>
+    <section class="modal-card-body">
+      <div class="media" v-for="(err, i) in dataErrors" :key="i">
+        <div class="media-left">
+          <span class="icon has-text-danger is-small">
+            <i class="fa fa-exclamation"></i>
+          </span>
+        </div>
+        <div class="media-content is-size-7">
+          <p>
+            <b>{{ err.category }}</b> - {{ getDate(err.createdAt) }}
+          </p>
+          <p><b>Profile ID:</b> {{ err.profileID }}</p>
+          <p>{{ err.error }}</p>
+        </div>
+      </div>
+    </section>
+    <footer class="modal-card-foot"></footer>
+  </div>
+</template>
+
+<script>
+import formatDateTime from '@/filters/formatDateTime.js'
+
+export default {
+  props: ['dataErrors'],
+  methods: {
+    getDate(date) {
+      return formatDateTime(date)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
