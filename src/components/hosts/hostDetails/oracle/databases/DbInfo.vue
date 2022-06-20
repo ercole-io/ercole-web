99fbd625010aad2f444058e1e6ce07c1edd0290e
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2022-06-20 15:58:48 +0200

    Fix boolean values into info section on host details databases

--- src/components/hosts/hostDetails/oracle/databases/DbInfo.vue
+++ src/components/hosts/hostDetails/oracle/databases/DbInfo.vue
@@ -1,26 +1,90 @@
 <template>
   <b-tab-item label="Info" v-if="hasInfo">
-    <div class="columns">
+    <div class="columns is-mobile is-multiline">
       <div class="column is-4">
-        <Card cardTitle="DB Details" cardType="custom">
-          <CardListHighlight :colSizes="['6', '6']" :list="details" />
-        </Card>
+        <ul class="db-info">
+          <li>Database Details</li>
+          <li v-if="dbInfo.status">
+            <span>Status</span>
+            <span>{{ dbInfo.status }}</span>
+          </li>
+          <li v-if="dbInfo.role">
+            <span>Role</span>
+            <span>{{ dbInfo.role }}</span>
+          </li>
+          <li v-if="dbInfo.dbID">
+            <span>Db ID</span>
+            <span>{{ dbInfo.dbID }}</span>
+          </li>
+          <li v-if="dbInfo.uniqueName">
+            <span>Unique Name</span>
+            <span>{{ dbInfo.uniqueName }}</span>
+          </li>
+          <li v-if="dbInfo.archivelog">
+            <span>Archive Log</span>
+            <SimpleBooleanIcon :value="dbInfo.archivelog" />
+          </li>
+          <li v-if="dbInfo.blockSize">
+            <span>Block Size</span>
+            <span>{{ dbInfo.blockSize }}</span>
+          </li>
+          <li v-if="dbInfo.charset">
+            <span>Charset</span>
+            <span>{{ dbInfo.charset }}</span>
+          </li>
+          <li v-if="dbInfo.nCharset">
+            <span>N Charset</span>
+            <span>{{ dbInfo.nCharset }}</span>
+          </li>
+        </ul>
       </div>
 
       <div class="column is-4">
         <div class="columns">
           <div class="column is-12">
-            <Card cardTitle="Memory" cardType="custom">
-              <CardListHighlight :colSizes="['6', '6']" :list="memory" />
-            </Card>
+            <ul class="db-info">
+              <li>Memory</li>
+              <li v-if="dbInfo.memoryTarget">
+                <span>Memory Target</span>
+                <SimpleBooleanIcon :value="dbInfo.memoryTarget" />
+              </li>
+              <li v-if="dbInfo.pgaTarget">
+                <span>PGA Target (GB)</span>
+                <span>{{ dbInfo.pgaTarget }}</span>
+              </li>
+              <li v-if="dbInfo.sgaMaxSize">
+                <span>SGA MaxSize (GB)</span>
+                <span>{{ dbInfo.sgaMaxSize }}</span>
+              </li>
+              <li v-if="dbInfo.sgaTarget">
+                <span>SGA Target (GB)</span>
+                <span>{{ dbInfo.sgaTarget }}</span>
+              </li>
+            </ul>
           </div>
         </div>
 
         <div class="columns">
           <div class="column is-12">
-            <Card cardTitle="Resource Utilization" cardType="custom">
-              <CardListHighlight :colSizes="['6', '6']" :list="resource" />
-            </Card>
+            <ul class="db-info">
+              <li>Resource Utilization</li>
+              <li v-if="dbInfo.dbTime">
+                <span>Db Time</span>
+                <span>{{ dbInfo.dbTime }}</span>
+              </li>
+              <li v-if="dbInfo.elapsed">
+                <span>Elapsed</span>
+                <span>{{ dbInfo.elapsed }}</span>
+              </li>
+              <li v-if="dbInfo.work">
+                <span>DB time / Elapsed (min. 1)</span>
+                <span>{{ dbInfo.work }}</span>
+              </li>
+              <li v-if="dbInfo.cpuCount">
+                <span>CPU Count</span>
+                <span>{{ dbInfo.cpuCount }}</span>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -28,17 +92,45 @@
       <div class="column is-4">
         <div class="columns">
           <div class="column is-12">
-            <Card cardTitle="Space Utilization" cardType="custom">
-              <CardListHighlight :colSizes="['6', '6']" :list="space" />
-            </Card>
+            <ul class="db-info">
+              <li>Space Utilization</li>
+              <li v-if="dbInfo.allocable">
+                <span>Allocable (GB)</span>
+                <span>{{ dbInfo.allocable }}</span>
+              </li>
+              <li v-if="dbInfo.allocable">
+                <span>DataFile Size (GB)</span>
+                <span>{{ dbInfo.allocable }}</span>
+              </li>
+              <li v-if="dbInfo.segmentsSize">
+                <span>Segments Size (GB)</span>
+                <span>{{ dbInfo.segmentsSize }}</span>
+              </li>
+            </ul>
           </div>
         </div>
 
         <div class="columns">
           <div class="column is-12">
-            <Card cardTitle="Additional Info" cardType="custom">
-              <CardListHighlight :colSizes="['6', '6']" :list="additional" />
-            </Card>
+            <ul class="db-info">
+              <li>Additional Info</li>
+              <li v-if="dbInfo.asm">
+                <span>ASM</span>
+                <SimpleBooleanIcon :value="dbInfo.asm" />
+              </li>
+              <li v-if="dbInfo.dataguard">
+                <span>Data Guard</span>
+                <SimpleBooleanIcon :value="dbInfo.dataguard" />
+              </li>
+              <li v-if="dbInfo.platform">
+                <span>Platform</span>
+                <span>{{ dbInfo.platform }}</span>
+              </li>
+              <li v-if="dbInfo.version">
+                <span>Version</span>
+                <span>{{ dbInfo.version }}</span>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -48,9 +140,9 @@
 
 <script>
 import { mapState } from 'vuex'
+import { mapBooleanIcon } from '@/helpers/helpers.js'
 import { filterOptionsOracle } from '@/helpers/hostDetails/filterOptions/oracle.js'
-import Card from '@/components/common/Card.vue'
-import CardListHighlight from '@/components/common/CardListHighlight.vue'
+import SimpleBooleanIcon from '@/components/common/SimpleBooleanIcon.vue'
 
 export default {
   props: {
@@ -60,66 +152,15 @@ export default {
     },
   },
   components: {
-    Card,
-    CardListHighlight,
+    SimpleBooleanIcon,
+  },
+  methods: {
+    bindIcon(value) {
+      return mapBooleanIcon(value)
+    },
   },
   computed: {
     ...mapState(['hostDetails']),
-    details() {
-      return [
-        { name: 'Status', value: this.dbInfo.status },
-        { name: 'Role', value: this.dbInfo.role },
-        { name: 'Db ID', value: this.dbInfo.dbID },
-        { name: 'Unique Name', value: this.dbInfo.uniqueName },
-        {
-          name: 'Archive Log',
-          value: this.dbInfo.archivelog ? 'yes' : 'no',
-        },
-        { name: 'Block Size', value: this.dbInfo.blockSize },
-        { name: 'Charset', value: this.dbInfo.charset },
-        { name: 'N Charset', value: this.dbInfo.nCharset },
-      ]
-    },
-    memory() {
-      return [
-        { name: 'Memory Target', value: this.dbInfo.memoryTarget },
-        { name: 'PGA Target (GB)', value: this.dbInfo.pgaTarget },
-        { name: 'SGA MaxSize (GB)', value: this.dbInfo.sgaMaxSize },
-        { name: 'SGA Target (GB)', value: this.dbInfo.sgaTarget },
-      ]
-    },
-    resource() {
-      return [
-        { name: 'Db Time', value: this.dbInfo.dbTime },
-        { name: 'Elapsed', value: this.dbInfo.elapsed },
-        { name: 'DB time / Elapsed (min. 1)', value: this.dbInfo.work },
-        { name: 'CPU Count', value: this.dbInfo.cpuCount },
-      ]
-    },
-    space() {
-      return [
-        {
-          name: 'Allocable (GB)',
-          value: this.dbInfo.allocable,
-        },
-        {
-          name: 'DataFile Size (GB)',
-          value: this.dbInfo.datafileSize,
-        },
-        {
-          name: 'Segments Size (GB)',
-          value: this.dbInfo.segmentsSize,
-        },
-      ]
-    },
-    additional() {
-      return [
-        { name: 'ASM', value: this.dbInfo.asm ? 'yes' : 'no' },
-        { name: 'Data Guard', value: this.dbInfo.dataguard ? 'yes' : 'no' },
-        { name: 'Platform', value: this.dbInfo.platform },
-        { name: 'Version', value: this.dbInfo.version },
-      ]
-    },
     hasInfo() {
       return (
         (this.hostDetails.selectedKeys.length === 1 &&
@@ -135,4 +176,36 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.db-info {
+  padding-top: 0;
+
+  li {
+    padding: 0 5px;
+    font-size: 0.7em;
+
+    span:first-child {
+      font-weight: 500;
+    }
+  }
+
+  li:not(:first-child) {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    align-items: center;
+    padding: 3px 6px;
+
+    &:nth-child(odd) {
+      background: #f1f1f1;
+    }
+  }
+
+  li:first-child {
+    box-shadow: 0 0.08em 0 lightslategrey;
+    font-weight: 600;
+    text-transform: uppercase;
+    padding-left: 0;
+  }
+}
+</style>
