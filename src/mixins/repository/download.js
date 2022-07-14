ea0b706d0eb1f34fcc10c8f7076842185c0a619b
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2022-07-14 20:11:14 +0200

    Repository list mode with download modal, more info, transiton between views and sorte feature

--- src/mixins/repository/download.js
+++ src/mixins/repository/download.js
@@ -1,33 +1,13 @@
-<template>
-  <td style="min-width: 0" v-tooltip="options(fileName)">
-    <span class="is-block has-text-centered" @click="callAction">
-      <b-icon :type="iconSet[0]" :pack="iconSet[1]" :icon="iconSet[2]" />
-    </span>
-  </td>
-</template>
-
-<script>
 import { bus } from '@/helpers/eventBus.js'
 import { saveAs } from 'file-saver'
 import axios from 'axios'
 import TooltipMixin from '@/mixins/tooltipMixin.js'
 import DownloadingModal from '@/components/common/DownloadingModal.vue'
-import { mapGetters } from 'vuex'
 
 export default {
   mixins: [TooltipMixin],
-  props: {
-    fileName: {
-      type: String,
-      default: '',
-    },
-    iconSet: {
-      type: Array,
-      required: true,
-    },
-  },
   methods: {
-    callAction() {
+    downloadRepo(downloadUrl, fileName) {
       const request = axios.CancelToken.source()
       bus.$on('callCancelExport', () => {
         request.cancel()
@@ -40,7 +20,7 @@ export default {
         props: {
           msgTxt: this.$i18n.t('common.general.wait'),
           btText: this.$i18n.t('common.general.cancelRequest'),
-          exportTitle: `Downloading ${this.fileName}`,
+          exportTitle: `Downloading ${fileName}`,
           exportType: 'download',
         },
         canCancel: false,
@@ -48,7 +28,7 @@ export default {
 
       setTimeout(() => {
         axios
-          .get(`${this.getRepoServiceBaseUrl}/all/${this.fileName}`, {
+          .get(downloadUrl, {
             cancelToken: request.token,
             responseType: 'blob',
             onDownloadProgress: (progressEvent) => {
@@ -59,7 +39,7 @@ export default {
             },
           })
           .then((res) => {
-            saveAs(res.data, this.fileName)
+            saveAs(res.data, fileName)
           })
           .then(() => {
             bus.$emit('callCloseModal')
@@ -67,10 +47,4 @@ export default {
       }, 1000)
     },
   },
-  computed: {
-    ...mapGetters(['getRepoServiceBaseUrl']),
-  },
 }
-</script>
-
-<style lang="scss" scoped></style>
