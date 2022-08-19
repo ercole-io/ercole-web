7de764359010866dc3a9aa0dc287fa43cf2c32f3
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2022-08-19 19:51:33 +0200

    Oracle databases services page

---
+++ src/store/modules/databases/oracle/services.js
@@ -0,0 +1,43 @@
+import _ from 'lodash'
+import { axiosRequest } from '@/services/services.js'
+
+export const state = () => ({
+  services: [],
+})
+
+export const getters = {
+  getOracleServices: (state, getters) => {
+    const services = []
+    _.map(state.services, (val) => {
+      services.push({
+        hostname: val.hostname,
+        dbName: val.databasename,
+        servName: val.oracleDatabaseService.name,
+        servEnabled: val.oracleDatabaseService.enabled,
+      })
+    })
+    return getters.filteredOrNot(services)
+  },
+}
+
+export const mutations = {
+  SET_SERVICES: (state, payload) => {
+    state.services = payload
+  },
+}
+
+export const actions = {
+  async getServices({ commit, dispatch }) {
+    dispatch('onLoadingTable')
+
+    const config = {
+      method: 'get',
+      url: '/hosts/technologies/oracle/databases/service-list',
+    }
+
+    await axiosRequest('baseApi', config).then((res) => {
+      dispatch('offLoadingTable')
+      commit('SET_SERVICES', res.data)
+    })
+  },
+}
