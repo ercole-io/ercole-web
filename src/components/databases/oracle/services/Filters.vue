6115855887fc55b31dd693e4e495ac7a6e495859
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2022-08-19 19:51:33 +0200

    Oracle databases services page

---
+++ src/components/databases/oracle/services/Filters.vue
@@ -0,0 +1,67 @@
+<template>
+  <AdvancedFiltersBase :submitAction="apply">
+    <Collapse :collapses="collapses">
+      <template slot="General">
+        <CustomField :label="$t('common.fields.hostname')">
+          <CustomAutocomplete
+            v-model="filters.hostname"
+            :filterResult="filteredhostname"
+            :filterMethod="setAutocompletes"
+          />
+        </CustomField>
+
+        <CustomField :label="$t('common.fields.name')">
+          <CustomAutocomplete
+            v-model="filters.dbName"
+            :filterResult="filtereddbName"
+            :filterMethod="setAutocompletes"
+          />
+        </CustomField>
+
+        <CustomField label="Service Name">
+          <CustomAutocomplete
+            v-model="filters.servName"
+            :filterResult="filteredservName"
+            :filterMethod="setAutocompletes"
+          />
+        </CustomField>
+
+        <CustomField label="Enabled">
+          <CustomRadio v-model="filters.servEnabled" />
+        </CustomField>
+      </template>
+    </Collapse>
+
+    <slot />
+  </AdvancedFiltersBase>
+</template>
+
+<script>
+import localFiltersMixin from '@/mixins/localFiltersMixin.js'
+import Collapse from '@/components/common/Collapse.vue'
+
+export default {
+  mixins: [localFiltersMixin],
+  components: {
+    Collapse,
+  },
+  data() {
+    return {
+      collapses: ['General'],
+      autocompletes: ['hostname', 'dbName', 'servName'],
+      filters: {
+        servEnabled: '',
+      },
+    }
+  },
+  methods: {
+    resetFilters() {
+      this.filters = {
+        servEnabled: '',
+      }
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
