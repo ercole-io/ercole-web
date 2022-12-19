4695b1cbb1004cbdd12cc3ec2d2406d36b9e3d57
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2022-12-19 13:54:46 +0100

    Fix table double scroll

--- src/components/hosts/hostDetails/microsoft/databases/dbs/FileGroups.vue
+++ src/components/hosts/hostDetails/microsoft/databases/dbs/FileGroups.vue
@@ -1,35 +1,33 @@
 <template>
-  <section class="wrap-table py-3">
-    <FullTable
-      :tableData="fileGroups"
-      :keys="keys"
-      hideSearch
-      hidePerpage
-      hidePagination
-      hideTopTable
-      :isLoadingTable="false"
-    >
-      <template slot="headData">
-        <v-th sortKey="alloc">Alloc</v-th>
-        <v-th sortKey="fileType">File Type</v-th>
-        <v-th sortKey="filename">File Name</v-th>
-        <v-th sortKey="growth">Growth</v-th>
-        <v-th sortKey="growthUnit">Growth Unit</v-th>
-        <v-th sortKey="status">Status</v-th>
-        <v-th sortKey="used">Used</v-th>
-      </template>
+  <FullTable
+    :tableData="fileGroups"
+    :keys="keys"
+    hideSearch
+    hidePerpage
+    hidePagination
+    hideTopTable
+    :isLoadingTable="false"
+  >
+    <template slot="headData">
+      <v-th sortKey="alloc">Alloc</v-th>
+      <v-th sortKey="fileType">File Type</v-th>
+      <v-th sortKey="filename">File Name</v-th>
+      <v-th sortKey="growth">Growth</v-th>
+      <v-th sortKey="growthUnit">Growth Unit</v-th>
+      <v-th sortKey="status">Status</v-th>
+      <v-th sortKey="used">Used</v-th>
+    </template>
 
-      <template slot="bodyData" slot-scope="rowData">
-        <TdContent :value="rowData.scope.alloc | prettyBytes(1, true)" />
-        <TdContent :value="rowData.scope.fileType" />
-        <TdContent :value="rowData.scope.filename" />
-        <TdContent :value="rowData.scope.growth" />
-        <TdContent :value="rowData.scope.growthUnit" />
-        <TdContent :value="rowData.scope.status" />
-        <TdContent :value="rowData.scope.used | prettyBytes(1, true)" />
-      </template>
-    </FullTable>
-  </section>
+    <template slot="bodyData" slot-scope="rowData">
+      <TdContent :value="rowData.scope.alloc | prettyBytes(1, true)" />
+      <TdContent :value="rowData.scope.fileType" />
+      <TdContent :value="rowData.scope.filename" />
+      <TdContent :value="rowData.scope.growth" />
+      <TdContent :value="rowData.scope.growthUnit" />
+      <TdContent :value="rowData.scope.status" />
+      <TdContent :value="rowData.scope.used | prettyBytes(1, true)" />
+    </template>
+  </FullTable>
 </template>
 
 <script>
