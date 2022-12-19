875435cca251244e2914a3cd572e78c16f7b4d7d
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2022-12-19 13:54:46 +0100

    Fix table double scroll

--- src/components/hosts/hostDetails/oracle/databases/DbPDBs.vue
+++ src/components/hosts/hostDetails/oracle/databases/DbPDBs.vue
@@ -62,136 +62,128 @@
           label="Schemas"
           v-if="pdb.pdbSchemas && pdb.pdbSchemas.length > 0"
         >
-          <section class="wrap-table py-3">
-            <FullTable
-              :tableData="pdb.pdbSchemas"
-              :keys="['user', 'total', 'tables', 'indexes', 'lob']"
-              hideSearch
-              hidePerpage
-              hidePagination
-              hideTopTable
-              :isLoadingTable="false"
-              style="overflow: hidden"
-            >
-              <template slot="headData">
-                <v-th sortKey="user">User</v-th>
-                <v-th sortKey="total">Total</v-th>
-                <v-th sortKey="tables">Tables</v-th>
-                <v-th sortKey="indexes">Indexes</v-th>
-                <v-th sortKey="lob">LOB</v-th>
-                <v-th sortKey="accountStatus">Account Status</v-th>
-              </template>
+          <FullTable
+            :tableData="pdb.pdbSchemas"
+            :keys="['user', 'total', 'tables', 'indexes', 'lob']"
+            hideSearch
+            hidePerpage
+            hidePagination
+            hideTopTable
+            :isLoadingTable="false"
+            style="overflow: hidden"
+          >
+            <template slot="headData">
+              <v-th sortKey="user">User</v-th>
+              <v-th sortKey="total">Total</v-th>
+              <v-th sortKey="tables">Tables</v-th>
+              <v-th sortKey="indexes">Indexes</v-th>
+              <v-th sortKey="lob">LOB</v-th>
+              <v-th sortKey="accountStatus">Account Status</v-th>
+            </template>
 
-              <template slot="bodyData" slot-scope="rowData">
-                <TdContent :value="rowData.scope.user" />
-                <TdContent :value="rowData.scope.total" />
-                <TdContent :value="rowData.scope.tables" />
-                <TdContent :value="rowData.scope.indexes" />
-                <TdContent :value="rowData.scope.lob" />
-                <TdContent :value="rowData.scope.accountStatus" />
-              </template>
-            </FullTable>
-          </section>
+            <template slot="bodyData" slot-scope="rowData">
+              <TdContent :value="rowData.scope.user" />
+              <TdContent :value="rowData.scope.total" />
+              <TdContent :value="rowData.scope.tables" />
+              <TdContent :value="rowData.scope.indexes" />
+              <TdContent :value="rowData.scope.lob" />
+              <TdContent :value="rowData.scope.accountStatus" />
+            </template>
+          </FullTable>
         </b-tab-item>
         <b-tab-item
           label="Tablespaces"
           v-if="pdb.pdbTablespaces && pdb.pdbTablespaces.length > 0"
         >
-          <section class="wrap-table py-3">
-            <FullTable
-              :tableData="pdb.pdbTablespaces"
-              :keys="['name', 'status', 'usedPerc', 'used', 'total', 'maxSize']"
-              hideSearch
-              hidePerpage
-              hidePagination
-              hideTopTable
-              :isLoadingTable="false"
-            >
-              <template slot="headData">
-                <v-th sortKey="name">Name</v-th>
-                <v-th sortKey="status">Status</v-th>
-                <v-th sortKey="usedPerc">Used</v-th>
-                <v-th sortKey="used">Used GB</v-th>
-                <v-th sortKey="total">Total</v-th>
-                <v-th sortKey="maxSize">Max Size</v-th>
-              </template>
+          <FullTable
+            :tableData="pdb.pdbTablespaces"
+            :keys="['name', 'status', 'usedPerc', 'used', 'total', 'maxSize']"
+            hideSearch
+            hidePerpage
+            hidePagination
+            hideTopTable
+            :isLoadingTable="false"
+          >
+            <template slot="headData">
+              <v-th sortKey="name">Name</v-th>
+              <v-th sortKey="status">Status</v-th>
+              <v-th sortKey="usedPerc">Used</v-th>
+              <v-th sortKey="used">Used GB</v-th>
+              <v-th sortKey="total">Total</v-th>
+              <v-th sortKey="maxSize">Max Size</v-th>
+            </template>
 
-              <template slot="bodyData" slot-scope="rowData">
-                <TdContent :value="rowData.scope.name" />
-                <TdContent :value="rowData.scope.status" />
-                <TdContent :value="rowData.scope.usedPerc" />
-                <TdContent :value="rowData.scope.used" />
-                <TdContent :value="rowData.scope.total" />
-                <TdContent :value="rowData.scope.maxSize" />
-              </template>
-            </FullTable>
-          </section>
+            <template slot="bodyData" slot-scope="rowData">
+              <TdContent :value="rowData.scope.name" />
+              <TdContent :value="rowData.scope.status" />
+              <TdContent :value="rowData.scope.usedPerc" />
+              <TdContent :value="rowData.scope.used" />
+              <TdContent :value="rowData.scope.total" />
+              <TdContent :value="rowData.scope.maxSize" />
+            </template>
+          </FullTable>
         </b-tab-item>
         <b-tab-item
           label="Grant Role"
           v-if="pdb.pdbGrantDba && pdb.pdbGrantDba.length > 0"
         >
-          <section class="wrap-table py-3">
-            <FullTable
-              :tableData="pdb.pdbGrantDba"
-              :keys="['adminOption', 'defaultRole', 'grantee']"
-              hideSearch
-              hidePerpage
-              hidePagination
-              hideTopTable
-              :isLoadingTable="false"
-            >
-              <template slot="headData">
-                <v-th sortKey="grantee">Grantee</v-th>
-                <v-th sortKey="adminOption">Admin Option</v-th>
-                <v-th sortKey="defaultRole">Default Role</v-th>
-              </template>
+          <FullTable
+            :tableData="pdb.pdbGrantDba"
+            :keys="['adminOption', 'defaultRole', 'grantee']"
+            hideSearch
+            hidePerpage
+            hidePagination
+            hideTopTable
+            :isLoadingTable="false"
+          >
+            <template slot="headData">
+              <v-th sortKey="grantee">Grantee</v-th>
+              <v-th sortKey="adminOption">Admin Option</v-th>
+              <v-th sortKey="defaultRole">Default Role</v-th>
+            </template>
 
-              <template slot="bodyData" slot-scope="rowData">
-                <TdContent :value="rowData.scope.grantee" />
-                <TdIcon :value="rowData.scope.adminOption" />
-                <TdIcon :value="rowData.scope.defaultRole" />
-              </template>
-            </FullTable>
-          </section>
+            <template slot="bodyData" slot-scope="rowData">
+              <TdContent :value="rowData.scope.grantee" />
+              <TdIcon :value="rowData.scope.adminOption" />
+              <TdIcon :value="rowData.scope.defaultRole" />
+            </template>
+          </FullTable>
         </b-tab-item>
         <b-tab-item
           label="Segment Advisors"
           v-if="pdb.segmentAdvisors && pdb.segmentAdvisors.length > 0"
         >
-          <section class="wrap-table py-3">
-            <FullTable
-              :tableData="pdb.segmentAdvisors"
-              :keys="[
-                'segmentOwner',
-                'segmentName',
-                'segmentType',
-                'partitionName',
-                'recommendation',
-              ]"
-              hideSearch
-              hidePerpage
-              hidePagination
-              hideTopTable
-              :isLoadingTable="false"
-            >
-              <template slot="headData">
-                <v-th sortKey="segmentOwner">Segment Owner</v-th>
-                <v-th sortKey="segmentName">Segment Name</v-th>
-                <v-th sortKey="segmentType">Segment Type</v-th>
-                <v-th sortKey="partitionName">Partition Name</v-th>
-                <v-th sortKey="recommendation">Recommendation</v-th>
-              </template>
+          <FullTable
+            :tableData="pdb.segmentAdvisors"
+            :keys="[
+              'segmentOwner',
+              'segmentName',
+              'segmentType',
+              'partitionName',
+              'recommendation',
+            ]"
+            hideSearch
+            hidePerpage
+            hidePagination
+            hideTopTable
+            :isLoadingTable="false"
+          >
+            <template slot="headData">
+              <v-th sortKey="segmentOwner">Segment Owner</v-th>
+              <v-th sortKey="segmentName">Segment Name</v-th>
+              <v-th sortKey="segmentType">Segment Type</v-th>
+              <v-th sortKey="partitionName">Partition Name</v-th>
+              <v-th sortKey="recommendation">Recommendation</v-th>
+            </template>
 
-              <template slot="bodyData" slot-scope="rowData">
-                <TdContent :value="rowData.scope.segmentOwner" />
-                <TdContent :value="rowData.scope.segmentName" />
-                <TdContent :value="rowData.scope.segmentType" />
-                <TdContent :value="rowData.scope.partitionName" />
-                <TdContent :value="rowData.scope.recommendation" />
-              </template>
-            </FullTable>
-          </section>
+            <template slot="bodyData" slot-scope="rowData">
+              <TdContent :value="rowData.scope.segmentOwner" />
+              <TdContent :value="rowData.scope.segmentName" />
+              <TdContent :value="rowData.scope.segmentType" />
+              <TdContent :value="rowData.scope.partitionName" />
+              <TdContent :value="rowData.scope.recommendation" />
+            </template>
+          </FullTable>
         </b-tab-item>
       </b-tabs>
     </b-collapse>
