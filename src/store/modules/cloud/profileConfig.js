8af921a0a5f984c331d4040f19c2c8737163a6e1
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2023-01-25 14:40:40 +0100

    Azure cloud profile post form

--- src/store/modules/cloud/profileConfig.js
+++ src/store/modules/cloud/profileConfig.js
@@ -77,6 +77,8 @@ export const actions = {
       url = oracleUrl
     } else if (getters.returnCloudTechnology === 'Aws') {
       url = awsUrl
+    } else if (getters.returnCloudTechnology === 'Azure') {
+      url = azureUrl
     }
 
     const config = {
