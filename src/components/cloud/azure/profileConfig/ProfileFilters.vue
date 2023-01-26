ff91f9b65dee910aef4ca8f1d5d5271caf27ff87
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2023-01-26 11:52:47 +0100

    Azure config profile filters

--- src/components/cloud/azure/profileConfig/ProfileFilters.vue
+++ src/components/cloud/azure/profileConfig/ProfileFilters.vue
@@ -1,9 +1,63 @@
 <template>
-  <div>Filters</div>
+  <AdvancedFiltersBase :submitAction="apply">
+    <Collapse :collapses="collapses">
+      <template slot="General">
+        <CustomField :label="$t('common.collumns.profileName')">
+          <CustomAutocomplete
+            v-model="filters.name"
+            :filterResult="filteredname"
+            :filterMethod="setAutocompletes"
+          />
+        </CustomField>
+
+        <CustomField label="Tenant ID">
+          <CustomSelect
+            v-model="filters.tenantid"
+            :options="filteredtenantid"
+          />
+        </CustomField>
+
+        <CustomField label="Client ID">
+          <CustomSelect
+            v-model="filters.clientid"
+            :options="filteredclientid"
+          />
+        </CustomField>
+
+        <CustomField label="Subscription ID">
+          <CustomSelect
+            v-model="filters.subscriptionid"
+            :options="filteredsubscriptionid"
+          />
+        </CustomField>
+
+        <CustomField :label="$t('common.collumns.region')">
+          <CustomSelect v-model="filters.region" :options="filteredregion" />
+        </CustomField>
+      </template>
+    </Collapse>
+
+    <slot />
+  </AdvancedFiltersBase>
 </template>
 
 <script>
-export default {}
+import localFiltersMixin from '@/mixins/localFiltersMixin.js'
+import Collapse from '@/components/common/Collapse.vue'
+
+export default {
+  mixins: [localFiltersMixin],
+  components: {
+    Collapse,
+  },
+  data() {
+    return {
+      collapses: ['General'],
+      autocompletes: ['name'],
+      selects: ['tenantid', 'clientid', 'subscriptionid', 'region'],
+    }
+  },
+}
 </script>
 
 <style lang="scss" scoped></style>
