869656af09492d3965aff7ae69f0c778125ce404
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2023-02-16 11:15:25 +0100

    First config files for SSO login

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -10,6 +10,17 @@
         </div>
         <div class="colunm">
           <LoginForm />
+
+          <hr style="background-color: #5fd8df; height: 1px" />
+
+          <div class="is-flex is-justify-content-center">
+            <b-button
+              @click="loginSSO"
+              style="background-color: #054759; color: white"
+            >
+              SSO Login
+            </b-button>
+          </div>
         </div>
       </div>
     </div>
@@ -19,6 +30,8 @@
 <script>
 import LoginForm from '@/components/login/FormLogin.vue'
 import ErcoleLogo from '@/components/common/ErcoleLogo.vue'
+import { mapActions } from 'vuex'
+import axios from 'axios'
 // import LocaleSwitcher from '@/components/LocaleSwitcher.vue'
 
 export default {
@@ -27,6 +40,26 @@ export default {
     LoginForm,
     ErcoleLogo,
     // LocaleSwitcher
+  },
+  methods: {
+    ...mapActions(['fetchConfigSSO']),
+    loginSSO() {
+      this.fetchConfigSSO().then((res) => {
+        const api = res.data.api_url
+        const client = res.data.client_id
+        const response = res.data.response_type
+        const redirect = res.data.redirect_uri
+        const scope = res.data.scope
+
+        axios
+          .get(
+            `${api}?client_id=${client}&response_type=${response}&redirect_uri=${redirect}&scope=${scope}`
+          )
+          .then((res) => {
+            console.log(res)
+          })
+      })
+    },
   },
 }
 </script>
