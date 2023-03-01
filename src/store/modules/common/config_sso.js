4a65ed22eddffd71390c75ce6ad6a312a8d5c716
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2023-03-01 14:49:05 +0100

    Single Sign On login feature

--- src/store/modules/common/config_sso.js
+++ src/store/modules/common/config_sso.js
@@ -18,7 +18,7 @@ import axiosConfigSSO from '@/axios/axios-config-sso.js'
 
 export const actions = {
   // eslint-disable-next-line no-empty-pattern
-  async fetchConfigSSO({}) {
+  async fetchConfigSSO() {
     return await axiosConfigSSO.get('/config_sso.json')
   },
 }
