1f0202d4e85481def605151f5d141581f2b9ab33
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2023-08-28 14:28:30 +0200

    Creating a component for databases and pdbs capacity

--- src/views/databases/oracle/Capacity.vue
+++ src/views/databases/oracle/Capacity.vue
@@ -33,93 +33,10 @@
           :label="host"
           :key="index"
         >
-          <b-tabs
-            size="is-small"
-            type="is-boxed"
-            destroy-on-hide
-            animated
+          <CapacityDatabases
+            :capacityData="oracleDatabasesCapacity"
             v-if="!loadingTableStatus"
-          >
-            <b-tab-item
-              v-for="db in oracleDatabasesCapacity"
-              :value="db.dbName"
-              :label="db.dbName"
-              :key="db.dbName"
-            >
-              <b-tabs size="is-small" type="is-toggle" class="p-5" expanded>
-                <b-tab-item
-                  v-for="cap in capacityTabs"
-                  :key="cap.id"
-                  :label="checkID(cap.id) ? `${cap.label} - Daily` : cap.label"
-                  :value="cap.id"
-                >
-                  <ChartBuilder
-                    chartType="line"
-                    :chartID="`${cap.id}-${cap.label}CapacityDailyChart`"
-                    :chartOptions="
-                      chartDailyOptions(cap.id, db.dbCapacityDaily)
-                    "
-                    :chartSeries="chartDailySeries(cap.id, db.dbCapacityDaily)"
-                    v-if="checkID(cap.id)"
-                  />
-
-                  <ChartBuilder
-                    chartType="bar"
-                    :chartID="`${cap.id}-${cap.label}CapacityChart`"
-                    :chartOptions="chartOptions(cap.label)"
-                    :chartSeries="getSeries(cap.id, db.dbCapacity)"
-                    v-if="!checkID(cap.id)"
-                  />
-                </b-tab-item>
-              </b-tabs>
-
-              <div class="pdb-box" v-if="db.pdbs && db.pdbs.length > 0">
-                <p class="has-text-centered has-text-weight-medium">
-                  {{ db.dbName }} - Pluggable Databases
-                </p>
-
-                <CollapseSimple
-                  :isOpen="false"
-                  :collapseID="`collapse-${pdb.pdbName}`"
-                  :collapseTitle="pdb.pdbName"
-                  v-for="pdb in db.pdbs"
-                  :key="pdb.pdbName"
-                >
-                  <b-tabs size="is-small" type="is-toggle" class="p-5" expanded>
-                    <b-tab-item
-                      v-for="capPdb in capacityTabs"
-                      :key="capPdb.id"
-                      :label="
-                        checkID(capPdb.id)
-                          ? `${capPdb.label} - Daily`
-                          : capPdb.label
-                      "
-                      :value="capPdb.id"
-                    >
-                      <ChartBuilder
-                        chartType="line"
-                        :chartID="`${capPdb.id}-${capPdb.label}CapacityDailyPdbChart`"
-                        :chartOptions="
-                          chartDailyOptions(capPdb.id, pdb.pdbCapacityDaily)
-                        "
-                        :chartSeries="
-                          chartDailySeries(capPdb.id, pdb.pdbCapacityDaily)
-                        "
-                        v-if="checkID(capPdb.id)"
-                      />
-                      <ChartBuilder
-                        chartType="bar"
-                        :chartID="`${capPdb.id}-${capPdb.label}CapacityPdbChart`"
-                        :chartOptions="chartOptions(capPdb.label)"
-                        :chartSeries="getSeries(capPdb.id, pdb.pdbCapacity)"
-                        v-if="!checkID(capPdb.id)"
-                      />
-                    </b-tab-item>
-                  </b-tabs>
-                </CollapseSimple>
-              </div>
-            </b-tab-item>
-          </b-tabs>
+          />
         </b-tab-item>
       </b-tabs>
 
@@ -137,23 +54,23 @@
 <script>
 import _ from 'lodash'
 import { mapActions, mapGetters, mapMutations } from 'vuex'
-import databasesCapacityMixin from '@/mixins/hostDetails/capacity/databasesCapacity.js'
-import databasesCapacityMonthMixin from '@/mixins/hostDetails/capacity/databasesCapacityMonth.js'
-import databasesCapacityDailyMixin from '@/mixins/hostDetails/capacity/databasesCapacityDaily.js'
+import CapacityDatabases from '@/components/databases/oracle/capacity/Databases.vue'
 import BoxContent from '@/components/common/BoxContent.vue'
 import SearchInput from '@/components/common/SearchInput.vue'
 import NoContent from '@/components/common/NoContent.vue'
-import CollapseSimple from '@/components/common/CollapseSimple.vue'
 import Loading from '@/components/common/Loading.vue'
 
 export default {
   name: 'oracle-databases-capacity-page',
-  mixins: [
-    databasesCapacityMixin,
-    databasesCapacityMonthMixin,
-    databasesCapacityDailyMixin,
-  ],
-  components: { BoxContent, SearchInput, NoContent, CollapseSimple, Loading },
+
+  components: {
+    CapacityDatabases,
+    BoxContent,
+    SearchInput,
+    NoContent,
+
+    Loading,
+  },
   data() {
     return {
       isMounted: false,
