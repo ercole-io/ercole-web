1d1075cc0a03ed3736ff9e645102b5f6242a2f00
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2023-08-28 11:48:45 +0200

    Invenrting collumns vcpu and ram usage

--- src/components/exadata/exadatas/ExadataProgress.vue
+++ src/components/exadata/exadatas/ExadataProgress.vue
@@ -1,6 +1,21 @@
 <template>
   <section class="columns mb-0">
     <div class="column is-6">
+      <span class="is-size-7 has-text-weight-semibold ml-2">VCPU Usage</span>
+      <ProgressBar
+        :progressMaxValue="exadataProgress.totalCPU"
+        :progressValue="exadataProgress.usedCPU"
+        :progressTooltip="
+          setTooltip(
+            exadataProgress.totalCPU,
+            exadataProgress.usedCPU,
+            exadataProgress.freeCPU,
+            ''
+          )
+        "
+      />
+    </div>
+    <div class="column is-6">
       <span class="is-size-7 has-text-weight-semibold ml-2">Ram Usage</span>
       <ProgressBar
         :progressMaxValue="exadataProgress.totalMemory"
@@ -18,21 +33,6 @@
       <p class="is-size-7 has-text-centered" v-else>
         {{ exadataProgress.totalMemory }} GB
       </p>
-    </div>
-    <div class="column is-6">
-      <span class="is-size-7 has-text-weight-semibold ml-2">VCPU Usage</span>
-      <ProgressBar
-        :progressMaxValue="exadataProgress.totalCPU"
-        :progressValue="exadataProgress.usedCPU"
-        :progressTooltip="
-          setTooltip(
-            exadataProgress.totalCPU,
-            exadataProgress.usedCPU,
-            exadataProgress.freeCPU,
-            ''
-          )
-        "
-      />
     </div>
   </section>
 </template>
