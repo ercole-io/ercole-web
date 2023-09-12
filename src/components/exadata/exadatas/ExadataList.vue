18b3917734cac8c1dcd5860b83dce255afeb2510
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2023-09-12 15:26:26 +0200

    Exadata switch view buttons

---
+++ src/components/exadata/exadatas/ExadataList.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Exadata List View</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/exadata/Exadata.vue
+++ src/views/exadata/Exadata.vue
@@ -2,7 +2,22 @@
   <section>
     <div class="columns">
       <div class="column is-12">
-        <ExadataTables />
+        <div class="contentChange">
+          <b-button
+            :type="changeLayout ? 'is-white' : 'is-primary'"
+            icon-pack="fas"
+            icon-left="list"
+            @click="updateLayout(changeLayout !== changeLayout)"
+          />
+          <b-button
+            :type="!changeLayout ? 'is-white' : 'is-primary'"
+            icon-pack="fas"
+            icon-left="table-cells-large"
+            @click="updateLayout(!changeLayout !== changeLayout)"
+          />
+        </div>
+        <ExadataList v-if="!changeLayout" />
+        <ExadataTables v-else />
       </div>
     </div>
   </section>
@@ -10,18 +25,28 @@
 
 <script>
 import { mapActions } from 'vuex'
+import ExadataList from '@/components/exadata/exadatas/ExadataList.vue'
 import ExadataTables from '@/components/exadata/exadatas/ExadataTables.vue'
 
 export default {
   name: 'engineered-page',
   components: {
     ExadataTables,
+    ExadataList,
+  },
+  data() {
+    return {
+      changeLayout: false,
+    }
   },
   async beforeMount() {
     await this.getExadataData()
   },
   methods: {
     ...mapActions(['getExadataData']),
+    updateLayout(val) {
+      this.changeLayout = val
+    },
   },
 }
 </script>
