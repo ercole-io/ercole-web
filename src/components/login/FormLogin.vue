d1ed05197d9be0761ba45a03e36e9c8dc9eddac1
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2023-11-27 15:44:26 +0100

    Setting up a background box in the login form

--- src/components/login/FormLogin.vue
+++ src/components/login/FormLogin.vue
@@ -1,80 +1,82 @@
 <template>
-  <form @submit.prevent="authLogin" class="login-form">
-    <CustomField>
-      <CustomSelect
-        v-model="loginType"
-        :options="['Basic Auth', 'LDAP']"
-        size=""
-        :hasReset="false"
-      />
-    </CustomField>
+  <div class="login-bgc">
+    <form @submit.prevent="authLogin" class="login-form">
+      <CustomField>
+        <CustomSelect
+          v-model="loginType"
+          :options="['Basic Auth', 'LDAP']"
+          size=""
+          :hasReset="false"
+        />
+      </CustomField>
 
-    <b-field
-      label-for="username"
-      :type="{
-        'is-danger': $v.username.$error,
-        'is-success': !$v.username.$invalid,
-      }"
-    >
-      <b-input
-        v-model="username"
-        icon-pack="fas"
-        icon="user"
-        :placeholder="$t('common.fields.username')"
-        @blur="$v.username.$touch()"
-        data-username
-      />
-      <template #message>
-        <div v-if="!$v.username.required && $v.username.$error" class="error">
-          {{ required($t('common.fields.username')) }}
-        </div>
-      </template>
-    </b-field>
+      <b-field
+        label-for="username"
+        :type="{
+          'is-danger': $v.username.$error,
+          'is-success': !$v.username.$invalid,
+        }"
+      >
+        <b-input
+          v-model="username"
+          icon-pack="fas"
+          icon="user"
+          :placeholder="$t('common.fields.username')"
+          @blur="$v.username.$touch()"
+          data-username
+        />
+        <template #message>
+          <div v-if="!$v.username.required && $v.username.$error" class="error">
+            {{ required($t('common.fields.username')) }}
+          </div>
+        </template>
+      </b-field>
 
-    <b-field
-      label-for="password"
-      :type="{
-        'is-danger': $v.password.$error,
-        'is-success': !$v.password.$invalid,
-      }"
-    >
-      <b-input
-        type="password"
-        v-model="password"
-        icon-pack="fas"
-        icon="lock"
-        :placeholder="$t('common.fields.password')"
-        @blur="$v.password.$touch()"
-        data-password
-        password-reveal
-      />
-      <template #message>
-        <div v-if="!$v.password.required && $v.password.$error" class="error">
-          {{ required($t('common.fields.password')) }}
-        </div>
-      </template>
-    </b-field>
+      <b-field
+        label-for="password"
+        :type="{
+          'is-danger': $v.password.$error,
+          'is-success': !$v.password.$invalid,
+        }"
+      >
+        <b-input
+          type="password"
+          v-model="password"
+          icon-pack="fas"
+          icon="lock"
+          :placeholder="$t('common.fields.password')"
+          @blur="$v.password.$touch()"
+          data-password
+          password-reveal
+        />
+        <template #message>
+          <div v-if="!$v.password.required && $v.password.$error" class="error">
+            {{ required($t('common.fields.password')) }}
+          </div>
+        </template>
+      </b-field>
 
-    <b-button
-      type="is-custom-primary"
-      native-type="submit"
-      :disabled="$v.$invalid"
-      expanded
-      data-submit
-    >
-      {{ $t('common.forms.signIn') }}
-    </b-button>
+      <b-button
+        type="is-custom-primary"
+        native-type="submit"
+        :disabled="$v.$invalid"
+        expanded
+        data-submit
+      >
+        {{ $t('common.forms.signIn') }}
+      </b-button>
 
-    <a
-      href="#"
-      @click="forgotPassModal"
-      class="is-flex is-justify-content-center has-text-grey-lighter mt-2"
-    >
-      Forgot Password
-    </a>
+      <a
+        href="#"
+        @click="forgotPassModal"
+        class="is-flex is-justify-content-center has-text-grey mt-4"
+      >
+        Forgot Password
+      </a>
 
-    <ErrorMsg />
-  </form>
+      <ErrorMsg />
+    </form>
+  </div>
 </template>
 
 <script>
@@ -150,6 +152,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.login-bgc {
+  background-color: rgba(255, 255, 255, 0.9);
+  border-radius: 10px;
+}
+
 .login-form {
   width: 500px;
   padding: 3rem;
@@ -157,7 +164,7 @@ export default {
   border-radius: 4px;
   background-color: transparent;
   margin-bottom: 0;
-  padding-bottom: 0;
+  padding-bottom: 20px;
 
   button[disabled] {
     opacity: 1;
