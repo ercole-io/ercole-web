7b5666556eb3929a7e19a6ee244929d8a3874ee3
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2024-02-07 16:01:07 +0100

    Changed ercole web version to 2.43.1

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -135,7 +135,7 @@ export default {
           title: 'Ercole Version',
           message: `
           <p class="has-text-weight-bold">Server Version: <span class="has-text-weight-normal">${this.version}</span></p>
-          <p class="has-text-weight-bold">Web Version: <span class="has-text-weight-normal">2.42.0</span></p>
+          <p class="has-text-weight-bold">Web Version: <span class="has-text-weight-normal">2.43.1</span></p>
         `,
           cancelButton: false,
           type: 'is-success',
