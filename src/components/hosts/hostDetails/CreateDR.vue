f4648b6a4e0b8428aa745bf3b529b982288b703a
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2025-04-01 16:40:00 +0200

    Changed modal info before clone host

--- src/components/hosts/hostDetails/CreateDR.vue
+++ src/components/hosts/hostDetails/CreateDR.vue
@@ -3,7 +3,7 @@
     :isLoading="loadingTableStatus"
     setHeight="30"
     setWidth="107"
-    v-if="!notCloned"
+    class="mr-2"
   >
     <b-button
       label="Clone Host"
@@ -12,7 +12,8 @@
       icon-pack="fas"
       icon-left="clone"
       size="is-small"
-      class="mr-2"
+      :disabled="hostAlreadyCloned || justCloned"
+      v-if="!notCloned"
     />
   </GhostLoading>
 </template>
@@ -30,41 +31,77 @@ export default {
   data() {
     return {
       isModalActive: false,
+      justCloned: false,
     }
   },
   methods: {
     ...mapActions(['hostCreateDrData']),
-    async cloneHost() {
+
+    async cloneHostAction() {
       try {
         await this.hostCreateDrData()
+        this.justCloned = true
+        this.$buefy.dialog.alert({
+          title: 'Clone Host',
+          message: `<p class="has-text-weight-medium">This Host <span class="has-text-weight-bold">${this.currentHost}</span> was already cloned.</p>
+                    <p class="has-text-weight-medium">If you want to clone this host again, go to <span class="has-text-weight-bold">${this.clonedHost}</span> and dismiss it.</p>`,
+          type: 'is-success',
+          hasIcon: true,
+          icon: 'clone',
+          iconPack: 'fas',
+          confirmText: `Go to ${this.clonedHost}`,
+          onConfirm: this.linkToClonedHost,
+          canCancel: true,
+          cancelText: 'Close',
+        })
       } catch (error) {
         if (error.response && error.response.status === 403) {
           this.$buefy.dialog.alert({
-            title: 'Clone Host',
-            message: `<p class="has-text-weight-medium">This Host <span class="has-text-weight-bold">${this.currentHost}</span> was already cloned.</p>
-                      <p class="has-text-weight-medium">If you want to clone this host again, go to <span class="has-text-weight-bold">${this.currentHost}-DR</span> and dismiss it.`,
-            confirmText: `Go to ${this.currentHost}-DR`,
-            onConfirm: this.linkToCLonedHost,
-            canCancel: true,
-            cancelText: 'Close',
-            type: 'is-success',
+            title: 'Clone Host Error',
+            message: `<p class="has-text-weight-medium">This Host was already cloned.</p>`,
+            confirmText: 'OK',
+            type: 'is-danger',
           })
+        } else {
+          console.error('Error cloning host:', error)
         }
       }
     },
-    linkToCLonedHost() {
+
+    cloneHost() {
+      this.$buefy.dialog.alert({
+        title: 'Clone Host',
+        message: `<p class="has-text-weight-medium">Are you sure you want to clone the host <span class="has-text-weight-bold">${this.currentHost}</span>?</p>`,
+        type: 'is-info',
+        hasIcon: true,
+        icon: 'clone',
+        iconPack: 'fas',
+        onConfirm: this.cloneHostAction,
+        canCancel: true,
+        cancelText: 'Close',
+      })
+    },
+
+    linkToClonedHost() {
       this.$router.push({
         name: 'hosts-details',
         params: {
-          hostname: `${this.currentHost}-DR`,
+          hostname: this.clonedHost,
         },
       })
     },
   },
   computed: {
-    ...mapGetters(['currentHost', 'loadingTableStatus']),
+    ...mapGetters(['currentHost', 'getHostnames', 'loadingTableStatus']),
+    clonedHost() {
+      return `${this.currentHost}_DR`
+    },
     notCloned() {
-      return _.includes(this.currentHost, '-DR')
+      return _.includes(this.currentHost, '_DR')
+    },
+
+    hostAlreadyCloned() {
+      return _.includes(this.getHostnames, this.clonedHost)
     },
   },
 }
