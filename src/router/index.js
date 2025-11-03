b6fee52505eead44533151f967b7df16f937a5c3
Ricardo Maiolini <ricardo.maiolini@gmail.com>
2025-11-03 12:15:43 +0100

    Hide scenarios from menu and change ercole version

--- src/router/index.js
+++ src/router/index.js
@@ -926,42 +926,42 @@ const routes = [
     },
     beforeEnter: verifyAuth,
   },
-  {
-    path: '/scenarios/create',
-    name: 'create-scenarios',
-    component: lazyRouteBuilder('scenarios/Create.vue'),
-    meta: {
-      label: 'Create Scenarios',
-      title: `${title}Create Scenarios`,
-      breadcrumb: [{ name: 'Scenarios' }, { name: 'Create' }],
-    },
-    beforeEnter: verifyAuth,
-  },
-  {
-    path: '/scenarios/list',
-    name: 'list-scenarios',
-    component: lazyRouteBuilder('scenarios/List.vue'),
-    meta: {
-      label: 'List Scenarios',
-      title: `${title}List Scenarios`,
-      breadcrumb: [{ name: 'Scenarios' }, { name: 'List' }],
-    },
-    beforeEnter: verifyAuth,
-  },
-  {
-    path: '/scenarios/details/:scenario/:id',
-    name: 'details-scenarios',
-    component: lazyRouteBuilder('scenarios/Details.vue'),
-    meta: {
-      label: 'Details Scenarios',
-      title: `${title}Details Scenarios`,
-      breadcrumb: [
-        { name: 'Scenarios', link: '/scenarios/list' },
-        { name: '' },
-      ],
-    },
-    beforeEnter: verifyAuth,
-  },
+  // {
+  //   path: '/scenarios/create',
+  //   name: 'create-scenarios',
+  //   component: lazyRouteBuilder('scenarios/Create.vue'),
+  //   meta: {
+  //     label: 'Create Scenarios',
+  //     title: `${title}Create Scenarios`,
+  //     breadcrumb: [{ name: 'Scenarios' }, { name: 'Create' }],
+  //   },
+  //   beforeEnter: verifyAuth,
+  // },
+  // {
+  //   path: '/scenarios/list',
+  //   name: 'list-scenarios',
+  //   component: lazyRouteBuilder('scenarios/List.vue'),
+  //   meta: {
+  //     label: 'List Scenarios',
+  //     title: `${title}List Scenarios`,
+  //     breadcrumb: [{ name: 'Scenarios' }, { name: 'List' }],
+  //   },
+  //   beforeEnter: verifyAuth,
+  // },
+  // {
+  //   path: '/scenarios/details/:scenario/:id',
+  //   name: 'details-scenarios',
+  //   component: lazyRouteBuilder('scenarios/Details.vue'),
+  //   meta: {
+  //     label: 'Details Scenarios',
+  //     title: `${title}Details Scenarios`,
+  //     breadcrumb: [
+  //       { name: 'Scenarios', link: '/scenarios/list' },
+  //       { name: '' },
+  //     ],
+  //   },
+  //   beforeEnter: verifyAuth,
+  // },
   {
     path: '/users',
     name: 'users',
